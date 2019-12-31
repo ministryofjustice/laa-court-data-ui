983cfffd147cff97cab4bbf2b978afb460dfc8a4
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-12-31 11:14:54 +0000

    Get govuk frontend styling working
    move stylesheets to webpack folder
    and amend manifests and config.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-
+// import '../stylesheets/application.scss';
+// require("@rails/ujs").start()
 // initialise govk design system
-const GOVUKFrontend = require('govuk-frontend')
-$(function() { GOVUKFrontend.initAll() })
+// const GOVUKFrontend = require('govuk-frontend')
+// $(function() { GOVUKFrontend.initAll() })
+
+require.context('govuk-frontend/govuk/assets');
+import '../stylesheets/application.scss';
+require("@rails/ujs").start();
+// require("turbolinks").start();
+// require("@rails/activestorage").start();
+// require("channels");
+initAll();
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
