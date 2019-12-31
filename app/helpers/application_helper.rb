3b4abf15017e9fb70c36119265ef5bb31c19b3e3
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-12-31 15:46:34 +0000

    add service name and page title helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def service_name
+    'Common platform UI'
+  end
+
+  def page_title(title = nil)
+    content_for :page_title, title || contextual_title
+  end
+
+  def contextual_title
+    page_title [controller_name, action_name].join(' ').titleize
+  end
 end
