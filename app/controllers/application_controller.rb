49093d9f0715884b2b130b8107c4f5191154939e
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-01-11 21:55:04 +0000

    enable devise at the application controller level
    all site activity requires login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   default_form_builder GOVUKDesignSystemFormBuilder::FormBuilder
   protect_from_forgery prepend: true, with: :exception
+  before_action :authenticate_user!
 
   def set_back_page_path
     session[:back_page_path] = request.path
