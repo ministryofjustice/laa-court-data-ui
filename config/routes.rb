6cd7551a566178c0b94ce25fb9177bff500c0e50
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-01-11 23:06:21 +0000

    Generate all devise controllers we may need
    For info and clarity. we could get rid
    of some or all of these if we do not need
    them (and rely on devises controllers directly).
    
    One need for these is to prevent user signup
    as, eventually, we want only admins to be able
    to register new users.

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   resources :search_filters, only: %i[new create]
   resources :searches, only: %i[new create]
 
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
 end
