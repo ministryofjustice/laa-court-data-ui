128ffa5c1dafdad6e651ed60aeb3ead49a20123a
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-01-11 18:12:35 +0000

    Generate and migrate devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root to: 'search_filters#new'
   resources :search_filters, only: %i[new create]
   resources :searches, only: %i[new create]
+
+  devise_for :users
 end
