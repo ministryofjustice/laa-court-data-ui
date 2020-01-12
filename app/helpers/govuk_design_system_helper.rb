3d24c9a5b821c627b14da9c013d15225d37bbe18
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-01-12 22:43:12 +0000

    Add govuk_design_system_helper and spec

---
+++ app/helpers/govuk_design_system_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# GOVUK GDS design system helpers
+# for producing markup of non-form
+# related markup.
+#
+module GovukDesignSystemHelper
+  def govuk_page_title(title = nil)
+    content_for :page_title, title || contextual_title
+
+    content_for :page_heading do
+      tag.h1(class: 'govuk-heading-xl') do
+        title || contextual_title
+      end
+    end
+  end
+
+  def contextual_title
+    [action_name.titleize, controller_name.downcase.singularize].join(' ')
+  end
+end
---
+++ spec/helpers/govuk_design_system_helper_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+RSpec.describe GovukDesignSystemHelper, type: :helper do
+  describe '#govuk_page_title' do
+    context 'when page title provided' do
+      before do
+        helper.govuk_page_title('My page title')
+      end
+
+      it 'stores :page_title' do
+        expect(helper.content_for(:page_title)).to include 'My page title'
+      end
+
+      it 'stores :page_heading' do
+        expect(helper.content_for(:page_heading)).to include 'My page title'
+      end
+
+      it ':page_title contains plain text' do
+        expect(helper.content_for(:page_title)).to eql 'My page title'
+      end
+
+      it ':page_heading contains GDS styled heading' do
+        markup = helper.content_for(:page_heading)
+        expect(markup).to eql '<h1 class="govuk-heading-xl">My page title</h1>'
+      end
+    end
+
+    context 'when no page title provided' do
+      before do
+        allow(controller).to receive(:controller_name).and_return 'Widgets'
+        allow(controller).to receive(:action_name).and_return 'show'
+        helper.govuk_page_title
+      end
+
+      it ':page_title contains title based on controller action' do
+        expect(helper.content_for(:page_title)).to eql 'Show widget'
+      end
+
+      it ':page_heading contains heading based on controller action' do
+        markup = helper.content_for(:page_heading)
+        expect(markup).to eql '<h1 class="govuk-heading-xl">Show widget</h1>'
+      end
+    end
+  end
+end
