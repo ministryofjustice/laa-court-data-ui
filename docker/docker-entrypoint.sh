e840be2de7d031f45b230299a1856550c692533a
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-01-20 08:09:28 +0000

    Add DB create to entry-point
    This should ideally be done by a
    k8s job.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+printf '\e[33mINFO: DB migrate\e[0m\n'
+RUBYOPT=-W:no-deprecated bundle exec rails db:create db:migrate
+
 # NOTE: "RUBYOPT=-W:no-deprecated" removes verbose
 # warnings raised by rails using ruby 2.7
 #
