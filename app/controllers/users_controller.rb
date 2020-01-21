64ded2cbc4c7181582a6a5a6687c86c4ad3ae57b
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2020-01-21 20:24:53 +0000

    Add update_password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,26 @@ class UsersController < ApplicationController
 
   before_action :set_user, only: %i[show edit update]
 
+  before_action :set_user, only: %i[show edit update change_password update_password]
+
   def show; end
 
   def edit; end
 
+  def change_password; end
+
+  def update_password
+    if current_user.update_with_password(user_params)
+      bypass_sign_in(@user)
+      redirect_to authenticated_root_path, notice: 'Password successfully updated'
+    else
+      render :change_password
+    end
+  end
+
   def update
     if @user.update!(user_params)
-      redirect_to authenticated_root_path
+      redirect_to authenticated_root_path, notice: 'User details successfully updated'
     else
       render :edit
     end
@@ -25,6 +38,12 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:last_name, :first_name, :email)
+    params.require(:user).permit(
+      :last_name,
+      :first_name,
+      :email,
+      :current_password,
+      :password,
+      :password_confirmation)
   end
 end
