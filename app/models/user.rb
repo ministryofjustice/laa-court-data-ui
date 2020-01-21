0dd16f06ccf28eac7a5c2f988631be9a83588db9
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-01-21 13:10:17 +0000

    Add user roles
    As part of authorization users will
    need to be assigned roles. Those
    roles will then be used to define
    what functionality is available
    for their use.
    
    Currently, I am defining roles
    caseworker, manager and admin.
    
    caseworker for search and self
    management; manager for creating
    deleting (and editing) other users;
    admin for product support team members
    only.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  include Roles
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise  :database_authenticatable,
@@ -11,6 +13,8 @@ class User < ApplicationRecord
           :trackable,
           :validatable,
           :lockable
+
+  accepts_roles :caseworker, :manager, :admin
 
   def name
     "#{first_name} #{last_name}"
