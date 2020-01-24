674a57d37bf15048e73dd780a9c3ee24223bd776
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-01-24 21:12:38 +0000

    Enable password unlock instructions (re)sending
    So users will receive an email to unlock
    themselves automatically when they lock
    themselves out. And to enable them
    to request the unlock email be sent
    again.

--- app/controllers/users/unlocks_controller.rb
+++ app/controllers/users/unlocks_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::UnlocksController < Devise::UnlocksController
+  skip_authorization_check only: %i[show new create]
+
   # GET /resource/unlock/new
   # def new
   #   super
