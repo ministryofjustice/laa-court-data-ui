01ae635f211c44c3c3669fb69b21fc1f2b5ff76f
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-02-03 20:53:18 +0000

    Add rspec helper pattern and fixture helpers
    As a central repository and pattern for adding
    rspec and other testing-related helpers.

---
+++ spec/support/application.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Custom application rspec helpers can/should
+# be added here.
+#
+module Application
+  module Helpers
+    def fixture_file(file, options = {})
+      base_path = ['spec', 'fixtures']
+      path = base_path.append(options[:relative_path]).append(file).compact
+      File.read(
+        Rails.root.join(*path)
+      )
+    end
+
+    def prosecution_cases_fixture(file, options = {})
+      fixture_file(file, relative_path: 'prosecution_cases')
+    end
+  end
+end
+
+
+RSpec.configure do |c|
+  c.include Application::Helpers
+end
