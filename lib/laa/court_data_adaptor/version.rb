a6ee5dba981c975a3b89ad287a4695537e9d6ab5
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-02-03 14:04:47 +0000

    delete LAA::CourtDataAdaptor

--- lib/laa/court_data_adaptor/version.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module LAA
-  module CourtDataAdaptor
-    VERSION = '0.0.1'
-    VERSION_RELEASED = '2020-03-31'
-    USER_AGENT = "laa-court-data-adaptor-client/#{VERSION}"
-  end
-end
