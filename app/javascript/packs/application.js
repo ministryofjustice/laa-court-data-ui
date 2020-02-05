2d139aab6c0c0ab69361c2a0ac57f80f6db09e69
Peter Hunt <naseberry@users.noreply.github.com>
2020-02-05 10:04:28 +0000

    Fix linting issues

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start();
-require("govuk-frontend").initAll();
-require.context('govuk-frontend/govuk/assets');
-import '../stylesheets/application.scss';
+import '../stylesheets/application.scss'
+require('@rails/ujs').start()
+require('govuk-frontend').initAll()
+require.context('govuk-frontend/govuk/assets')
 // require("@rails/activestorage").start();
 // require("channels");
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
