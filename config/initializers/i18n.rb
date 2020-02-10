c6f00fd96f017319fbe64bdd0afe527503f767ca
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-02-10 16:50:43 +0000

    Make load_path OS agnostic

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
+# Note that appending directly to I18n.load_paths instead of to
+# the application's configured i18n will not override translations
+# from external gems.
+# see https://guides.rubyonrails.org/i18n.html#configure-the-i18n-module
+
 I18n.load_path = I18n.load_path +
-                 Dir[File.expand_path('config/locales') + '/**/*.yml']
+                 Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
