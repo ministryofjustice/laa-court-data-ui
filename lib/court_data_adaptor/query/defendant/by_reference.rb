8236ae79367202768e2653523ac737ae0ad998e9
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-03-06 21:31:37 +0000

    Add defendant reference parseer
    To "clean" user input and determine whether a reference
    is a national insurance number or not

--- lib/court_data_adaptor/query/defendant/by_reference.rb
+++ lib/court_data_adaptor/query/defendant/by_reference.rb
@@ -9,23 +9,37 @@ module CourtDataAdaptor
         def call
           refresh_token_if_required!
 
-          cases = resource
-                  .includes(:defendants)
-                  .where(
-                    national_insurance_number: term
-                  )
-                  .all
+          reference = ReferenceParser.new(term)
+          if reference.national_insurance_number?
+            cases = resource
+                    .includes(:defendants)
+                    .where(
+                      national_insurance_number: reference.value
+                    )
+                    .all
+          else
+            cases = resource
+                    .includes(:defendants)
+                    .where(
+                      arrest_summons_number: reference.value
+                    )
+                    .all
+          end
 
           matching_defendants(cases)
         end
 
         private
 
+        def reference
+          @reference ||= ReferenceParser.new(term)
+        end
+
         def matching_defendants(cases)
           cases.each_with_object([]) do |c, results|
             c.defendants.each do |d|
               d.prosecution_case_reference = c.prosecution_case_reference
-              results << d if d.national_insurance_number.casecmp(term).zero?
+              results << d if d.national_insurance_number.casecmp(reference.value).zero?
             end
           end
         end
