3488259b711ef12f60272da07d67d1adb511323c
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-03-09 10:27:56 +0000

    add arrest summons filter and refactor by_reference search

--- lib/court_data_adaptor/query/defendant/by_reference.rb
+++ lib/court_data_adaptor/query/defendant/by_reference.rb
@@ -9,27 +9,22 @@ module CourtDataAdaptor
         def call
           refresh_token_if_required!
 
-          reference = ReferenceParser.new(term)
-          if reference.national_insurance_number?
-            cases = resource
-                    .includes(:defendants)
-                    .where(
-                      national_insurance_number: reference.value
-                    )
-                    .all
-          else
-            cases = resource
-                    .includes(:defendants)
-                    .where(
-                      arrest_summons_number: reference.value
-                    )
-                    .all
-          end
+          cases = resource
+                  .includes(:defendants)
+                  .where(
+                    **clause
+                  )
+                  .all
 
           matching_defendants(cases)
         end
 
         private
+
+        def clause
+          key = reference.national_insurance_number? ? :national_insurance_number : :arrest_summons_number
+          { key => reference.value }
+        end
 
         def reference
           @reference ||= ReferenceParser.new(term)
@@ -39,7 +34,11 @@ module CourtDataAdaptor
           cases.each_with_object([]) do |c, results|
             c.defendants.each do |d|
               d.prosecution_case_reference = c.prosecution_case_reference
-              results << d if d.national_insurance_number.casecmp(reference.value).zero?
+              if reference.national_insurance_number?
+                results << d if d.national_insurance_number.casecmp(reference.value).zero?
+              else
+                results << d if d.arrest_summons_number.casecmp(reference.value).zero?
+              end
             end
           end
         end
