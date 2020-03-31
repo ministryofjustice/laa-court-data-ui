d076e8e277bda9d22ebd52ebe3214f0001364ea2
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2020-03-31 09:34:54 +0100

    Refactor Raven config

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,10 +2,10 @@
 
 require 'raven'
 
-if %w[production].include?(Rails.env) && ENV['SENTRY_DSN'].present?
+if Rails.env.eql?('production') && ENV['SENTRY_DSN'].present?
   Raven.configure do |config|
     config.dsn = ENV['SENTRY_DSN']
-    config.environments = %w[production]
+    config.environments = Rails.env
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   end
 end
