c1dea560d3f4401aeb0fcc2ab86f5b292cbacf24
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-06-15 15:55:32 +0100

    Add devise user:username field
    To be used to store an MLRA like
    username that will be sent with requests
    to the adaptor for unlinking (at least),
    and from there to apps such as MLRA for
    auditing purposes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_130904) do
+ActiveRecord::Schema.define(version: 2020_06_15_145140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,9 +34,11 @@ ActiveRecord::Schema.define(version: 2020_01_21_130904) do
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "roles", default: ["caseworker"], array: true
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
