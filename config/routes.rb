c455e8011c95a66f229186e1bf6102d7f552b6a2
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-06-16 22:36:58 +0100

    Remove remnants of devise registerable
    Was kept in in case it would be needed
    and to ensure devise defaults would allow
    registration. This can not be removed
    and therefore not impact test coverage.

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     confirmations: 'users/confirmations',
     passwords: 'users/passwords',
-    registrations: 'users/registrations',
     sessions: 'users/sessions',
     unlocks: 'users/unlocks'
   }
