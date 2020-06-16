8ff0afa9cc1a36e65ae2ce71e4a526837987279f
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-06-16 17:48:25 +0100

    refactor unlinking feature test

--- spec/features/unlinking_a_defendant_from_maat_spec.rb
+++ spec/features/unlinking_a_defendant_from_maat_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Unlinking a defendant from MAAT', type: :feature do
   let(:defendant_asn) { '0TSQT1LMI7CR' }
   let(:api_url) { ENV['COURT_DATA_ADAPTOR_API_URL'] }
 
-  context 'when signed in' do
+  context 'when viewing defendant details' do
     let(:user) { create(:user) }
 
     before do
@@ -16,68 +16,65 @@ RSpec.describe 'Unlinking a defendant from MAAT', type: :feature do
       stub_request(:get, "#{api_url}/prosecution_cases")
         .with(query: query)
         .to_return(body: body, headers: json_api_header)
+
+      visit "defendants/#{defendant_asn}"
     end
 
-    context 'when viewing a case' do
-      before do
-        visit "defendants/#{defendant_asn}"
+    context 'with unlinked defendant' do
+      let(:defendant_fixture) { 'unlinked/defendant_by_reference_body.json' }
+
+      it 'asks the user to link to a MAAT ID' do
+        expect(page).to have_content('Enter the MAAT ID')
+      end
+    end
+
+    context 'with linked defendant' do
+      let(:defendant_fixture) { 'linked/defendant_by_reference_body.json' }
+
+      it 'does not display the link to a MAAT ID field' do
+        expect(page).not_to have_field('MAAT ID')
+        expect(page).not_to have_content('Enter the MAAT ID')
       end
 
-      context 'with no link' do
-        let(:defendant_fixture) { 'unlinked/defendant_by_reference_body.json' }
-
-        it 'asks the user to link to a MAAT ID' do
-          expect(page).to have_content('Enter the MAAT ID')
-        end
+      it 'displays the remove MAAT ID link' do
+        expect(page).to have_link('Remove link to court data')
+        expect(page).to have_govuk_warning('Removing the link will stop hearing updates being received')
       end
 
-      context 'with a pre-existing link' do
-        let(:defendant_fixture) { 'linked/defendant_by_reference_body.json' }
-
-        it 'does not display the link to a MAAT ID field' do
-          expect(page).not_to have_field('MAAT ID')
-          expect(page).not_to have_content('Enter the MAAT ID')
-        end
-
-        it 'displays the remove MAAT ID link' do
-          expect(page).to have_link('Remove link to court data')
-          expect(page).to have_govuk_warning('Removing the link will stop hearing updates being received')
-        end
-
-        context 'when unlinking the defendant' do
-          let(:defendant_id_from_fixture) { '41fcb1cd-516e-438e-887a-5987d92ef90f' }
-          let(:path) { "#{api_url}/defendants/#{defendant_id_from_fixture}" }
-          let(:payload) do
+      context 'when unlinking the defendant' do
+        let(:defendant_id_from_fixture) { '41fcb1cd-516e-438e-887a-5987d92ef90f' }
+        let(:path) { "#{api_url}/defendants/#{defendant_id_from_fixture}" }
+        let(:payload) do
+          {
+            data:
             {
-              data:
-              {
-                id: defendant_id_from_fixture,
-                type: 'defendants',
-                attributes: {
-                  prosecution_case_reference: 'TEST12345',
-                  user_name: user.username,
-                  unlink_reason_code: 1,
-                  unlink_reason_text: 'Wrong MAAT ID'
-                }
+              id: defendant_id_from_fixture,
+              type: 'defendants',
+              attributes: {
+                prosecution_case_reference: 'TEST12345',
+                user_name: user.username,
+                unlink_reason_code: 1,
+                unlink_reason_text: 'Wrong MAAT ID'
               }
             }
-          end
+          }
+        end
 
-          before do
-            stub_request(:patch, path)
-            click_on 'Remove link to court data'
-          end
+        before do
+          stub_request(:patch, path)
+          click_on 'Remove link to court data'
+        end
 
-          # TODO: this is more of a request spec and should be moved there
-          it 'sends an unlink request to the adapter' do
-            expect(a_request(:patch, path)
-              .with(body: payload.to_json))
-              .to have_been_made
-          end
+        # TODO: this is more of a request spec and should be moved there
+        it 'sends an unlink request to the adapter' do
+          expect(a_request(:patch, path)
+            .with(body: payload.to_json))
+            .to have_been_made
+        end
 
-          it 'displays flash notice' do
-            expect(page).to have_govuk_flash(:notice, text: 'You have successfully unlinked from the court data source')
-          end
+        it 'displays flash notice' do
+          expect(page).to \
+            have_govuk_flash(:notice, text: 'You have successfully unlinked from the court data source')
         end
       end
     end
