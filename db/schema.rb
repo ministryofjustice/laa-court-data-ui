717de1193afa86c1996d567f1584ce3634ed783c
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-06-19 13:28:13 +0100

    Change username `null: false`
    Required for ulinking, at least

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_145140) do
+ActiveRecord::Schema.define(version: 2020_06_19_115713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_06_15_145140) do
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "roles", default: ["caseworker"], array: true
-    t.string "username"
+    t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
