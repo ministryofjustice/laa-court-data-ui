21fd66adfe415104509c92064492da7fa8d4596e
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2020-07-06 09:39:01 +0100

    Add link_attempt model and associated error validation messages
    Tweak laa_references_controller
    
    Fix rubocop warnings

--- app/controllers/laa_references_controller.rb
+++ app/controllers/laa_references_controller.rb
@@ -1,14 +1,27 @@
 # frozen_string_literal: true
 
 class LaaReferencesController < ApplicationController
+  before_action :set_defendant_asn_if_required,
+                :load_and_authorize_search
+  before_action :set_defendant_if_required
+
   def create
     authorize! :create, :link_maat_reference, message: I18n.t('unauthorized.default')
-    if link_laa_reference
-      flash[:notice] = I18n.t('laa_reference.link.success')
+    set_link_attempt
+    if @link_attempt.valid?
+      if link_laa_reference
+        flash[:notice] = I18n.t('laa_reference.link.success')
+      else
+        flash[:alert] = I18n.t('laa_reference.link.failure', error_messages: error_messages)
+      end
+      redirect_to edit_defendant_path(defendant_asn)
     else
-      flash[:alert] = I18n.t('laa_reference.link.failure', error_messages: error_messages)
+      render 'defendants/edit'
     end
-    redirect_to edit_defendant_path(laa_reference_params[:id])
+  end
+
+  def defendant
+    @defendant ||= @search.execute.first
   end
 
   private
@@ -26,7 +39,8 @@ class LaaReferencesController < ApplicationController
     params.permit(
       :id,
       :defendant_id,
-      :maat_reference
+      :maat_reference,
+      link_attempt: %i[id defendant_id maat_reference defendant_asn]
     )
   end
 
@@ -35,7 +49,11 @@ class LaaReferencesController < ApplicationController
   end
 
   def resource_params
-    @resource_params ||= laa_reference_params.select! { |k, _v| k.in? %w[maat_reference defendant_id] }
+    @resource_params ||= if link_attempt_params
+                           link_attempt_params.select! { |k, _v| k.in? %w[maat_reference defendant_id] }
+                         else
+                           laa_reference_params.select! { |k, _v| k.in? %w[maat_reference defendant_id] }
+                         end
   end
 
   def no_maat_id?
@@ -44,5 +62,44 @@ class LaaReferencesController < ApplicationController
 
   def error_messages
     @errors.map { |k, v| "#{k.humanize} #{v.join(', ')}" }.join("\n")
+  end
+
+  def load_and_authorize_search
+    @search = Search.new(filter: 'defendant_reference', term: @defendant_asn)
+    authorize! :create, @search
+  end
+
+  def set_defendant_if_required
+    defendant
+  end
+
+  def link_attempt_params
+    laa_reference_params[:link_attempt]
+  end
+
+  def link_attempt_attributes
+    if link_attempt_params
+      link_attempt_params.merge(no_maat_id: no_maat_id?)
+    else
+      laa_reference_params.merge(no_maat_id: no_maat_id?)
+    end
+  end
+
+  def set_link_attempt
+    @link_attempt = if link_attempt_attributes
+                      LinkAttempt.new(link_attempt_attributes)
+                    else
+                      LinkAttempt.new
+                    end
+  end
+
+  def set_defendant_asn_if_required
+    defendant_asn
+  end
+
+  def defendant_asn
+    @defendant_asn ||= laa_reference_params[:id]\
+                   || link_attempt_params[:defendant_asn]\
+                   || link_attempt_params[:id]
   end
 end
---
+++ app/models/link_attempt.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class LinkAttempt
+  include ActiveModel::Model
+  attr_accessor :id, :maat_reference, :defendant_id, :defendant_asn, :no_maat_id
+
+  validates :maat_reference,
+            format: { with: /\A[0-9]{7}\z/, unless: :no_maat_id }
+end
