f223f99a812ba03873169c2bfea4ab9a98c00cc8
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2020-07-20 09:30:19 +0100

    Add court data adaptor query by_uuid
    Add by_uuid to court_data_adaptor query
    
    add by_uuid to search_spec

---
+++ lib/court_data_adaptor/query/defendant/by_uuid.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module CourtDataAdaptor
+    module Query
+      module Defendant
+        class ByUuid < Base
+          acts_as_resource CourtDataAdaptor::Resource::ProsecutionCase
+  
+          def call
+            refresh_token_if_required!
+  
+            cases = resource
+                    .includes(:defendants)
+                    .where(
+                      uuid: uuid
+                    )
+                    .all
+  
+            matching_defendants(cases)
+          end
+  
+          private
+  
+          def uuid
+            term
+          end
+  
+          def matching_defendants(cases)
+            cases.each_with_object([]) do |c, results|
+              c.defendants.each do |d|
+                d.prosecution_case_reference = c.prosecution_case_reference
+                results << d if d.send(reference.kind).casecmp(uuid).zero?
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  
