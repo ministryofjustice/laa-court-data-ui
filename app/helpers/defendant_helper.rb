690bdb41d1365d04a4e3f3328658e0129161f596
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2020-07-20 11:45:49 +0100

    Use id instead of asn or nino in defendant_helper

--- app/helpers/defendant_helper.rb
+++ app/helpers/defendant_helper.rb
@@ -3,9 +3,9 @@
 module DefendantHelper
   def defendant_link_path(defendant)
     if defendant.linked?
-      edit_defendant_path(defendant.arrest_summons_number || defendant.national_insurance_number)
+      edit_defendant_path(defendant.id)
     else
-      new_laa_reference_path(id: defendant.arrest_summons_number || defendant.national_insurance_number)
+      new_laa_reference_path(id: defendant.id)
     end
   end
 end
--- spec/helpers/defendant_helper_spec.rb
+++ spec/helpers/defendant_helper_spec.rb
@@ -10,24 +10,24 @@ RSpec.describe DefendantHelper, type: :helper do
     # rubocop:disable RSpec/VerifiedDoubles
     let(:defendant) { double(defendant_class) }
     # rubocop:enable RSpec/VerifiedDoubles
-    let(:arrest_summons_number) { 'ABCD1EFG2HIJ' }
+    let(:id) { '12abc3de-456f-789g-012h-3456i78jk90l' }
 
     context 'when defendant linked' do
       before do
         allow(defendant).to receive(:linked?).and_return(true)
-        allow(defendant).to receive(:arrest_summons_number).and_return(arrest_summons_number)
+        allow(defendant).to receive(:id).and_return(id)
       end
 
-      it { is_expected.to eql "/defendants/#{arrest_summons_number}/edit" }
+      it { is_expected.to eql "/defendants/#{id}/edit" }
     end
 
     context 'when defendant not linked' do
       before do
         allow(defendant).to receive(:linked?).and_return(false)
-        allow(defendant).to receive(:arrest_summons_number).and_return(arrest_summons_number)
+        allow(defendant).to receive(:id).and_return(id)
       end
 
-      it { is_expected.to eql "/laa_references/new?id=#{arrest_summons_number}" }
+      it { is_expected.to eql "/laa_references/new?id=#{id}" }
     end
   end
 end
