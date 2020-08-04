756e3aa05c6d21b77c05023d8af5e5dad9e6016a
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-08-04 22:44:55 +0100

    Increase coverage for court data adaptor base spec

--- spec/lib/court_data_adaptor/query/base_spec.rb
+++ spec/lib/court_data_adaptor/query/base_spec.rb
@@ -15,4 +15,30 @@ RSpec.describe CourtDataAdaptor::Query::Base do
   end
 
   it_behaves_like 'court_data_adaptor query object'
+
+  describe '.call' do
+    subject(:call) { test_class.call('whatever') }
+
+    let(:instance) { instance_double(test_class) }
+
+    before do
+      allow(test_class).to receive(:new).and_return instance
+      allow(instance).to receive(:call)
+      call
+    end
+
+    it 'initializes new instance with args' do
+      expect(test_class).to have_received(:new).with('whatever', {})
+    end
+
+    it 'sends #call to instance' do
+      expect(instance).to have_received(:call)
+    end
+  end
+
+  describe '#call' do
+    subject(:call) { described_class.new('whatever').call }
+
+    it { expect { call }.to raise_error StandardError, /Implement in subclass/ }
+  end
 end
