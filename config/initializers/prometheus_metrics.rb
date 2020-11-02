c879b1af2165e519dccdd27c7c6219326bc2bc36
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2020-11-02 14:09:23 +0000

    Set host for prometheus-exporter

--- config/initializers/prometheus_metrics.rb
+++ config/initializers/prometheus_metrics.rb
@@ -4,6 +4,10 @@ unless Rails.env.test?
   require 'prometheus_exporter/instrumentation'
   require 'prometheus_exporter/middleware'
 
+  PrometheusExporter::Client.default = PrometheusExporter::Client.new(
+    host: 'laa-court-data-ui-metrics-service'
+  )
+
   PrometheusExporter::Instrumentation::Process.start(type: 'master')
   Rails.application.middleware.unshift PrometheusExporter::Middleware
 end
