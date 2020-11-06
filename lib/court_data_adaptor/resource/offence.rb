2b4a2686e0dc25741c857a44064a62f878ad5372
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-11-06 08:58:06 +0000

    Display plea and date of plea for defendant offence
    Need by caseworkers. Note that the adaptor
    still needs to be amended to include changes
    of plea in which case this is likely to change.

--- lib/court_data_adaptor/resource/offence.rb
+++ lib/court_data_adaptor/resource/offence.rb
@@ -12,6 +12,11 @@ module CourtDataAdaptor
       property :plea_date, type: :string
       property :mode_of_trial, type: :string
       property :mode_of_trial_reason, type: :string
+
+      def plea_and_date
+        return if plea.nil?
+        "#{plea&.humanize} on #{plea_date&.to_date&.strftime('%d/%m/%Y')}"
+      end
     end
   end
 end
