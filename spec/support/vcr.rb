ef27431b8000ef4386c684a7e43129febeb39407
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-11-14 19:48:45 +0000

    Enable VCR debugging
    vcr can now be debug using
    ```
    VCR_DEBUG=whatever rspec spec/whatever_spec.rb
    ```
    
    logs written to `log/vcr.log`

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,6 +3,7 @@
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
+  config.debug_logger = File.open('log/vcr.log', 'w') if ENV['VCR_DEBUG']
   config.before_record do |i|
     i.response.body.force_encoding('UTF-8')
   end
