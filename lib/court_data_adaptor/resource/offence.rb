fe1b32b8a181c29647ed3007412b958bb5c3dd0d
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-12-01 10:30:03 +0000

    Add custom jsonapi object caster
    To enable casting of jsonapi client attributes
    of arrays of hashes to be converted to a collection of
    objects.

--- lib/court_data_adaptor/resource/offence.rb
+++ lib/court_data_adaptor/resource/offence.rb
@@ -8,15 +8,9 @@ module CourtDataAdaptor
       belongs_to :defendant
 
       property :title, type: :string
-      property :plea, type: :string
-      property :plea_date, type: :string
+      property :pleas, type: :struct_collection, default: []
       property :mode_of_trial, type: :string
       property :mode_of_trial_reason, type: :string
-
-      def plea_and_date
-        return if plea.nil?
-        "#{plea&.humanize} on #{plea_date&.to_date&.strftime('%d/%m/%Y')}"
-      end
     end
   end
 end
