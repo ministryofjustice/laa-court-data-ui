a0f2a669b767b5a6f79d7e8bf1329418a3173d50
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-01-07 14:02:37 +0000

    Explicitly define relations and properties
    Since we rely on these properties and relations to exist
    it is clearer to define then. Calling these
    methods on the object will return nil, rather than undefined
    method for testing purposes too.

--- lib/court_data_adaptor/resource/hearing.rb
+++ lib/court_data_adaptor/resource/hearing.rb
@@ -6,7 +6,13 @@ module CourtDataAdaptor
       acts_as_resource self
 
       has_many :providers
+      has_many :hearing_events
+
+      property :court_name, type: :string
       property :defendant_names, default: []
+      property :hearing_type, type: :string
+      property :judge_names, default: []
+      property :prosecution_advocate_names, default: []
     end
   end
 end
--- spec/lib/court_data_adaptor/resource/hearing_spec.rb
+++ spec/lib/court_data_adaptor/resource/hearing_spec.rb
@@ -3,6 +3,16 @@
 require 'court_data_adaptor'
 
 RSpec.describe CourtDataAdaptor::Resource::Hearing do
+  let(:relations) { %i[providers hearing_events] }
+
+  let(:properties) do
+    %i[defendant_names
+       hearing_type
+       court_name
+       prosecution_advocate_names
+       judge_names]
+  end
+
   it_behaves_like 'court_data_adaptor acts_as_resource object', resource: described_class do
     let(:klass) { described_class }
     let(:instance) { described_class.new }
@@ -14,8 +24,8 @@ RSpec.describe CourtDataAdaptor::Resource::Hearing do
     let(:instance) { described_class.new }
   end
 
-  it { is_expected.to respond_to(:providers) }
-  it { is_expected.to respond_to(:defendant_names) }
+  it { is_expected.to respond_to(*relations) }
+  it { is_expected.to respond_to(*properties) }
 
   describe '#defendant_names' do
     subject { instance.defendant_names }
