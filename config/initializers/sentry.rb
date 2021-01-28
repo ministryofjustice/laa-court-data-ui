32bd3a1cdbb40e89cdc3e35128634411cd1c1c42
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2021-01-28 08:20:36 +0000

    Pin gemfile versions

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,6 @@
 if Rails.env.eql?('production') && ENV['SENTRY_DSN'].present?
   Sentry.init do |config|
     config.dsn = ENV['SENTRY_DSN']
-    config.traces_sample_rate = 0.2
     config.breadcrumbs_logger = [:active_support_logger]
   end
 end
