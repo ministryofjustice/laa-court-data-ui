0e3fba9d9d51eead0f61800b3a9c43bc17c8f96c
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-02-02 09:53:47 +0000

    Extract hearing attendees to partial
    Continuation of extraction
    of logical view segments to partials for
    isolation of funtionality and for testing
    purposes.

--- spec/views/hearings/show.html.haml_spec.rb
+++ spec/views/hearings/show.html.haml_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe 'hearings/show.html.haml', type: :view do
-  include RSpecHtmlMatchers
+  subject(:render_view) { render }
 
   let(:hearing) { CourtDataAdaptor::Resource::Hearing.new }
 
@@ -11,46 +11,8 @@ RSpec.describe 'hearings/show.html.haml', type: :view do
     allow(hearing).to receive(:hearing_events).and_return([])
   end
 
-  context 'with defendant_names' do
-    before { allow(hearing).to receive(:defendant_names).and_return(['Joe Bloggs', 'Fred Dibnah']) }
-
-    it 'displays defendant names with line breaks' do
-      render
-      expect(rendered).to have_tag('td.govuk-table__cell', text: /Joe Bloggs.*Fred Dibnah/) do
-        with_tag(:br)
-      end
-    end
-  end
-
-  context 'with prosecution_advocate_names' do
-    before do
-      allow(hearing)
-        .to receive(:prosecution_advocate_names)
-        .and_return(['Percy Prosecutor', 'Linda Lawless'])
-    end
-
-    it 'displays prosecution advocate names with line breaks' do
-      render
-      expect(rendered).to have_tag('td.govuk-table__cell', text: /Percy Prosecutor.*Linda Lawless/) do
-        with_tag(:br)
-      end
-    end
-  end
-
-  context 'with judge_names' do
-    before do
-      allow(hearing)
-        .to receive(:judge_names)
-        .and_return(['Mr Justice Pomfrey', 'Ms Justice Arden'])
-    end
-
-    it 'displays judge names with line breaks' do
-      render
-      expect(rendered).to have_tag('td.govuk-table__cell', text: /Mr Justice Pomfrey.*Ms Justice Arden/) do
-        with_tag(:br)
-      end
-    end
-  end
+  it { is_expected.to render_template(:_attendees) }
+  it { is_expected.to render_template(:_hearing_events) }
 
   context 'with cracked_ineffective_trial' do
     let(:cracked_ineffective_trial) do
@@ -66,15 +28,8 @@ RSpec.describe 'hearings/show.html.haml', type: :view do
         .and_return(cracked_ineffective_trial)
     end
 
-    it 'displays result row "header"' do
-      render
-      expect(rendered).to have_tag('th.govuk-table__header', text: /Result/)
-    end
-
-    it 'displays cracked_ineffective_trial type and reason sentence' do
-      render
-      expect(rendered).to have_tag('td.govuk-table__cell', text: /Ineffective: Another case over-ran/)
-    end
+    it { is_expected.to have_selector('th.govuk-table__header', text: /Result/) }
+    it { is_expected.to have_selector('td.govuk-table__cell', text: /Ineffective: Another case over-ran/) }
   end
 
   context 'without cracked_ineffective_trial' do
@@ -86,9 +41,6 @@ RSpec.describe 'hearings/show.html.haml', type: :view do
         .and_return(cracked_ineffective_trial)
     end
 
-    it 'does not display result row "header"' do
-      render
-      expect(rendered).not_to have_tag('th.govuk-table__header', text: /Result/)
-    end
+    it { is_expected.not_to have_selector('th.govuk-table__header', text: /Result/) }
   end
 end
