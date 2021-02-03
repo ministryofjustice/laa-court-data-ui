edcae9f3ef0e76308a643a8590a06056853ae4af
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-02-03 08:53:09 +0000

    Extract prosecution case defendants to partial
    Continuation of extraction of logical view segments
    to partials for isolation of funtionality and for testing
    purposes.

---
+++ spec/views/prosecution_cases/show.html.haml_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.describe 'prosecution_cases/show.html.haml', type: :view do
+  subject(:render_view) { render }
+
+  let(:results) { [prosecution_case] }
+
+  let(:prosecution_case) do
+    CourtDataAdaptor::Resource::ProsecutionCase
+      .new(prosecution_case_reference: 'THECASEURN')
+  end
+
+  before do
+    allow(view).to receive(:govuk_page_title).and_return 'A Gov uk page title'
+    allow(prosecution_case).to receive(:hearings).and_return([])
+    allow(prosecution_case).to receive(:defendants).and_return([])
+    assign(:prosecution_case, prosecution_case)
+    assign(:results, results)
+  end
+
+  it { is_expected.to have_content('A Gov uk page title') }
+  it { is_expected.to render_template(:_cracked_ineffective_trial) }
+  it { is_expected.to render_template(:_defendants) }
+  it { is_expected.to render_template(:_hearing_summaries) }
+end
