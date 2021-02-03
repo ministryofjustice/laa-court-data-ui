d512daa75e829cf5c0a7c7070dda604762d3123e
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-02-03 08:55:39 +0000

    Explicitly define required properties
    These properties of the defendant
    are essential to the correct functioning
    of the UI and therefore should be defined
    for clarity.
    
    The app requires certain properties to be
    either readable or read/writable. The property
    definition itself (from json_api_client gem)
    will create accessors, but the app only needs
    certain attributes the be assignable/writable.

--- lib/court_data_adaptor/resource/defendant.rb
+++ lib/court_data_adaptor/resource/defendant.rb
@@ -7,8 +7,11 @@ module CourtDataAdaptor
 
       has_many :offences
 
+      property :id, type: :string
       property :prosecution_case_reference, type: :string
       property :name, type: :string
+      property :date_of_birth, type: :string
+      property :maat_reference, type: :string
       property :user_name
       property :unlink_reason_code
       property :unlink_reason_text
