d1325ca823ba2fcb4ba7239f54653b4d17ac1905
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-02-12 15:32:39 +0000

    add `api_url` stub_helper

--- spec/support/stub_helper.rb
+++ spec/support/stub_helper.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+def api_url
+  CourtDataAdaptor.configuration.api_url
+end
+
 def load_json_stub(relative_path)
   path = Rails.root.join('spec', 'fixtures', 'stubs', relative_path)
   File.read(path)
