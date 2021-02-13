40846471cef18275759c8616f6769107c58dfe58
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-02-13 15:39:40 +0000

    Remove repeated action_mailer.perform_caching option
    This is repeated below

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,8 +54,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "laa_court_data_ui_production"
 
-  config.action_mailer.perform_caching = false
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
