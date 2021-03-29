b4fe92170cb683c26e01e1924a2d8a8e3f6fa5b4
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-03-29 16:11:40 +0100

    Modify feature spec in line with search changes

--- spec/features/search/defendant_by_name_spec.rb
+++ spec/features/search/defendant_by_name_spec.rb
@@ -7,27 +7,59 @@ RSpec.feature 'Defendant by name and dob search', type: :feature, js: true do
     sign_in user
   end
 
-  scenario 'with one result', :vcr do
+  scenario 'with full name search', :vcr do
     visit '/'
 
     choose 'A defendant by name and date of birth'
     click_button 'Continue'
-    fill_in 'search-term-field', with: 'Josefa Franecki'
-    fill_in 'search_dob_3i', with: '15'
-    fill_in 'search_dob_2i', with: '06'
-    fill_in 'search_dob_1i', with: '1961'
+    fill_in 'search-term-field', with: 'trever glover'
+    fill_in 'search_dob_3i', with: '1'
+    fill_in 'search_dob_2i', with: '1'
+    fill_in 'search_dob_1i', with: '1990'
+
     click_button 'Search'
 
     expect(page).to have_text(
-      'Search results for "Josefa Franecki, 15 June 1961"'
+      'Search results for "trever glover, 01 January 1990"'
     )
-    expect(page).to have_field('Defendant name', with: 'Josefa Franecki')
-    expect(page).to have_field('Day', with: '15')
-    expect(page).to have_field('Month', with: '6')
-    expect(page).to have_field('Year', with: '1961')
+    expect(page).to have_field('Defendant name', with: 'trever glover')
+    expect(page).to have_field('Day', with: '1')
+    expect(page).to have_field('Month', with: '1')
+    expect(page).to have_field('Year', with: '1990')
 
     within 'tbody.govuk-table__body' do
-      expect(page).to have_content('Josefa Franecki', minimum: 1)
+      expect(page).to have_content('Trever Glover', minimum: 1)
+      expect(page).to have_content('Wilford Glover', minimum: 1)
+      expect(page).to have_content('Danial GLOVER', minimum: 1)
+      expect(page).to have_content('Aliya GLOVER', minimum: 1)
+      expect(page).to have_content('Evert GLOVER', minimum: 1)
+    end
+
+    expect(page).to be_accessible.within '#main-content'
+  end
+
+  scenario 'with partial name search', :vcr do
+    visit '/'
+
+    choose 'A defendant by name and date of birth'
+    click_button 'Continue'
+    fill_in 'search-term-field', with: 'trever'
+    fill_in 'search_dob_3i', with: '01'
+    fill_in 'search_dob_2i', with: '01'
+    fill_in 'search_dob_1i', with: '1990'
+    click_button 'Search'
+
+    expect(page).to have_text(
+      'Search results for "trever, 01 January 1990"'
+    )
+    expect(page).to have_field('Defendant name', with: 'trever')
+    expect(page).to have_field('Day', with: '1')
+    expect(page).to have_field('Month', with: '1')
+    expect(page).to have_field('Year', with: '1990')
+
+    within 'tbody.govuk-table__body' do
+      expect(page).to have_content('Trever', minimum: 1)
+      expect(page).to have_content('Trever', maximum: 1)
     end
 
     expect(page).to be_accessible.within '#main-content'
