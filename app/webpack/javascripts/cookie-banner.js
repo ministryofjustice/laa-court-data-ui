fe990f671f60eac8cb375f9cc5a761597e1971b7
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2021-05-27 09:55:03 +0100

    AAC-68 Cookie Pages and Banners

--- app/webpack/javascripts/cookie-banner.js
+++ app/webpack/javascripts/cookie-banner.js
@@ -4,12 +4,15 @@ import {
 import {
   setConsentedToCookie,
   checkConsentedToCookieExists,
+  cookieConsent,
+  removeOptionalCookies,
   removeAllPreviousUsedCookies
 } from './cookie-helper'
 
 export default class CookieBanner {
   constructor () {
     this.$module = document.querySelector('[data-module="app-cookie-banner"]')
+    this.$form = document.querySelector('#cookie-form')
 
     // If the page doesn't have the banner then stop
     if (!this.$module) {
@@ -25,6 +28,12 @@ export default class CookieBanner {
       this.showCookieMessage()
       this.bindEvents()
     }
+
+    if (!cookieConsent()) {
+      removeOptionalCookies(cookieConsent())
+    }
+
+    this.checkCookies()
   }
 
   bindEvents () {
@@ -40,6 +49,7 @@ export default class CookieBanner {
 
   declineCookie () {
     this.hideCookieMessage()
+    removeOptionalCookies(false)
     setConsentedToCookie(false)
   }
 
@@ -49,5 +59,20 @@ export default class CookieBanner {
 
   hideCookieMessage () {
     this.$module.classList.add('app-cookie-banner--hidden')
+  }
+
+  checkCookies () {
+    if (this.$form) {
+      this.$on = document.querySelector('#ga_on')
+      this.$off = document.querySelector('#ga_off')
+      this.$get = window.location.search.substr(1)
+      // Set cookies as per parameter
+      if (this.$get) {
+        this.$get === 'analytical-cookies=off' ? this.declineCookie() : this.$get === 'analytical-cookies=on' && this.acceptCookie();
+      }
+      // Set radio to cookie value
+      this.$off.setAttribute('checked', 'checked')
+      cookieConsent() && this.$on.setAttribute('checked', 'checked')
+    }
   }
 }
