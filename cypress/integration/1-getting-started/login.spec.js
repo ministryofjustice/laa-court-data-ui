282119575a015cb4bbe467d56992520f03553c84
Mabel Li <mabel.li@digital.justice.gov.uk>
2021-07-16 17:18:43 +0100

    Fix JS linter complaints by adding global vars to package.json

--- cypress/integration/1-getting-started/login.spec.js
+++ cypress/integration/1-getting-started/login.spec.js
@@ -1,18 +1,18 @@
 describe('User Login Page', () => {
-    beforeEach(() => {
-      cy.visit('/')
-    })
+  beforeEach(() => {
+    cy.visit('/')
+  })
 
-    it('displays the dev banner', () => {
-        cy.get('.govuk-phase-banner__content').should('contain', Cypress.env('environment'))
-    })
+  it('displays the dev banner', () => {
+    cy.get('.govuk-phase-banner__content').should('contain', Cypress.env('environment'))
+  })
 
-    it('displays the login page', () => {
-        cy.get('.govuk-heading-xl').should('have.text', 'Sign in')
-        cy.get('#new_user')
-          .should('contain', 'Username or email')
-          .and('contain', 'Password')
-        cy.get('input#user-login-field').should.exist
-        cy.get('input#user-password-field').should.exist
-    })
+  it('displays the login page', () => {
+    cy.get('.govuk-heading-xl').should('have.text', 'Sign in')
+    cy.get('#new_user')
+      .should('contain', 'Username or email')
+      .and('contain', 'Password')
+    cy.get('input#user-login-field').should('exist')
+    cy.get('input#user-password-field').should('exist')
+  })
 })
