01277c604377ef1dabe3730fe72b47212497c79e
Obsiye <mrabdirizak@gmail.com>
2021-08-26 09:59:44 +0100

    Add sentry performance monitoring
    Set the sample rate to 0.5% which is 0.05 for traces_sample_rate

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,5 +4,8 @@ if Rails.env.eql?('production') && ENV['SENTRY_DSN'].present?
   Sentry.init do |config|
     config.dsn = ENV['SENTRY_DSN']
     config.breadcrumbs_logger = [:active_support_logger]
+
+    # Sample rate is set to 5%
+    config.traces_sample_rate = 0.05
   end
 end
