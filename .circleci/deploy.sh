2b6d4ddd5d2c423c63c73b06cc8ca0e9216e1804
Paul King <paul.king@digital.justice.gov.uk>
2021-12-17 21:00:19 +0000

    Update deploy script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -62,6 +62,7 @@ function _circleci_deploy() {
   login="$(AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} aws ecr get-login --no-include-email)"
   ${login}
 
+  printf '\e[33mK8S Login...\e[0m\n'
   echo -n ${K8S_CLUSTER_CERT} | base64 -d > ./ca.crt
   kubectl config set-cluster ${K8S_CLUSTER_NAME} --certificate-authority=./ca.crt --server=${K8S_CLUSTER_URL}
   kubectl config set-credentials circleci --token=${K8S_TOKEN}
@@ -69,18 +70,22 @@ function _circleci_deploy() {
   kubectl config use-context ${K8S_CLUSTER_NAME}
   kubectl --namespace=${K8S_NAMESPACE} get pods
 
+  printf '\e[33mFormulating Image name...\e[0m\n'
   docker_image_tag=${ECR_ENDPOINT}/${GITHUB_TEAM_NAME_SLUG}/${REPO_NAME}:app-${CIRCLE_SHA1}
 
   # decrypt and apply secrets first so changes can be picked up by deployment
+  printf '\e[33mDecrypting and Applying Secrets...\e[0m\n'
   echo "${GIT_CRYPT_KEY}" | base64 -d > git-crypt.key
   git-crypt unlock git-crypt.key
   kubectl apply -f .k8s/${environment}/secrets.yaml 2> /dev/null
 
   # apply deployment with specfied image
+  printf '\e[33mDeploying Image...\e[0m\n'
   kubectl set image -f .k8s/${cluster}/${environment}/deployment.yaml laa-court-data-ui-app=${docker_image_tag} laa-court-data-ui-metrics=${docker_image_tag} --local -o yaml | kubectl apply -f -
   kubectl set image -f .k8s/${cluster}/${environment}/deployment-worker.yaml laa-court-data-ui-worker=${docker_image_tag} laa-court-data-ui-metrics=${docker_image_tag} --local --output yaml | kubectl apply -f -
 
   # apply non-image specific config
+  printf '\e[33mApplying Config...\e[0m\n'
   kubectl apply \
   -f .k8s/${cluster}/${environment}/service.yaml \
   -f .k8s/${cluster}/${environment}/ingress.yaml
