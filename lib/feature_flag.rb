9dabffdec553dd47d507dd718345b6b055accc52
Paul King <paul.king@digital.justice.gov.uk>
2022-03-24 23:30:40 +0000

    Add implementation of Feature Flag class

---
+++ lib/feature_flag.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Feature
+  def self.enabled?(feature_name)
+    flag = ENV.fetch(feature_name.upcase.to_s, false)
+    ActiveModel::Type::Boolean.new.cast(flag)
+  end
+end
---
+++ spec/lib/feature_flag_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'feature_flag'
+
+RSpec.describe Feature do
+  context 'when environment variable is not set' do
+    before do
+      ENV['TEST_VAR'] = nil
+    end
+
+    it 'returns false' do
+      expect(described_class.enabled?(:test_var)).to be false
+    end
+  end
+
+  context 'when environment variable is set to true' do
+    before do
+      ENV['TEST_VAR'] = 'true'
+    end
+
+    it 'returns true' do
+      expect(described_class.enabled?(:test_var)).to be true
+    end
+  end
+
+  context 'when environment variable is set to false' do
+    before do
+      ENV['TEST_VAR'] = 'false'
+    end
+
+    it 'returns false' do
+      expect(described_class.enabled?(:test_var)).to be false
+    end
+  end
+end
