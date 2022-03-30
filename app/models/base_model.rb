7ebcf0f4db7f4a64e9e916e1c74c0d1f6964661d
Paul King <paul.king@digital.justice.gov.uk>
2022-03-30 21:10:44 +0100

    Add base model for ActiveResource

---
+++ app/models/base_model.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'active_resource'
+
+class BaseModel < ActiveResource::Base
+  self.site = Rails.configuration.x.court_data_api_config.uri
+  self.user = Rails.configuration.x.court_data_api_config.user
+  self.password = Rails.configuration.x.court_data_api_config.secret
+  self.include_format_in_path = false
+  self.format = :json
+  headers['Content-Type'] = 'application/json'
+end
