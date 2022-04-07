dac157d96d0606029bb09b2c9efbd5b42aec9d8a
Paul King <paul.king@digital.justice.gov.uk>
2022-04-07 14:03:59 +0100

    Fix bad styling in test

--- spec/requests/linking/link_defendant_maat_reference_v2_spec.rb
+++ spec/requests/linking/link_defendant_maat_reference_v2_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe 'link defendant maat reference', type: :request, vcr: true, stub_
     let(:maat_invalid_uuid) do
       {
         title: 'Unable to link the defendant using the MAAT ID.',
-        message: 'Defendant is not a valid uuid, MAAT reference 1234567'\
+        message: 'Defendant is not a valid uuid, MAAT reference 1234567 '\
                  'has no data created against Maat application.'
       }
     end
