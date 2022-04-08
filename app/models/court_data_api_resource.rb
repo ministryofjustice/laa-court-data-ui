42fc2b9c2e4ddc7818a357f13027f65556ad0120
Paul King <paul.king@digital.justice.gov.uk>
2022-04-08 17:00:24 +0100

    Add comment to module

--- app/models/court_data_api_resource.rb
+++ app/models/court_data_api_resource.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 # rubocop:disable Style/OptionalBooleanParameter
-
+# Module has been implemented to assist with testing ActiveResource. The site property is not
+# loaded as expected during tests so this module assists with refreshing the value so that we
+# do not receive a null value error.
 module CourtDataApiResource
   attr_accessor :active_record_fields_set
 
