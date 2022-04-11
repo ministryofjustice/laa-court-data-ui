611239793a8d71de629d950b5cd5e60d6b7b3643
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2022-04-11 13:20:08 +0100

    Added unlink_reason_code to laa ref model

--- app/models/laa_references.rb
+++ app/models/laa_references.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class LaaReferences < BaseModel
-  attr_accessor :defendant_id, :user_name, :maat_reference
+  attr_accessor :defendant_id, :user_name, :maat_reference, :unlink_reason_code
 end
