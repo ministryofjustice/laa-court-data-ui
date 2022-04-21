36123d6d4eea9fabf35188bd8bafbae7a8d21146
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2022-04-21 09:32:13 +0100

    Inherit from CDA::...::ReferenceParser
    Inherit the ReferenceParser from CDA in CdApi::ReferenceParserService and override the #kind method
    , which is different between v1 and v2 (:arrest_summons_number becomes :asn)

--- app/services/cd_api/reference_parser_service.rb
+++ app/services/cd_api/reference_parser_service.rb
@@ -1,22 +1,7 @@
 # frozen_string_literal: true
 
 module CdApi
-  class ReferenceParserService
-    # rubocop:disable Layout/LineLength
-    NINO_REGEXP = /^(?!BG)(?!GB)(?!NK)(?!KN)(?!TN)(?!NT)(?!ZZ)(?:[A-CEGHJ-PR-TW-Z][A-CEGHJ-NPR-TW-Z])[0-9]{6}[A-D]{1}$/
-    # rubocop:enable Layout/LineLength
-
-    def initialize(term)
-      @original_value = term
-      @value = @original_value.delete("\s\t\r\n\/\-").upcase
-    end
-
-    attr_reader :original_value, :value
-
-    def national_insurance_number?
-      value.match?(NINO_REGEXP)
-    end
-
+  class ReferenceParserService < CourtDataAdaptor::Query::Defendant::ReferenceParser
     def kind
       national_insurance_number? ? :nino : :asn
     end
