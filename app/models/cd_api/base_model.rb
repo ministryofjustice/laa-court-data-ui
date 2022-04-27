bc1da6e9e7dde948cf5f1b32024b06f226dedb84
Paul King <paul.king@digital.justice.gov.uk>
2022-04-27 22:08:04 +0100

    Allow us to send a correlationId with each request

--- app/models/cd_api/base_model.rb
+++ app/models/cd_api/base_model.rb
@@ -3,11 +3,21 @@
 require 'active_resource'
 module CdApi
   class BaseModel < ActiveResource::Base
+    cattr_accessor :static_headers
+    self.static_headers = headers
+
     self.site = Rails.configuration.x.court_data_api_config.uri
     self.user = Rails.configuration.x.court_data_api_config.user
     self.password = Rails.configuration.x.court_data_api_config.secret
     self.include_format_in_path = false
     self.format = :json
     headers['Content-Type'] = 'application/json'
+    headers['Laa-Transaction-Id'] = SecureRandom.uuid
+
+    def self.headers
+      new_headers = static_headers.clone
+      new_headers['Laa-Transaction-Id'] = SecureRandom.uuid
+      new_headers
+    end
   end
 end
