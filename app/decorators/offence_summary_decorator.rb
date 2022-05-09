006691020b0114ae6a67a3a410244d50ab3a2a33
Paul King <paul.king@digital.justice.gov.uk>
2022-05-09 13:23:25 +0100

    Fix linting issues

--- app/decorators/offence_summary_decorator.rb
+++ app/decorators/offence_summary_decorator.rb
@@ -2,8 +2,13 @@
 
 class OffenceSummaryDecorator < BaseDecorator
   def plea_sentence
-    return t('generic.not_available') if plea.blank? || plea.value.blank?
+    return t('generic.not_available') if plea&.value.blank?
+    return_plea plea
+  end
 
+  private
+
+  def return_plea(plea)
     t('offence.plea.sentence',
       plea: plea&.value&.humanize || t('generic.not_available'),
       pleaded_at: plea&.date&.to_date&.strftime('%d/%m/%Y') || t('generic.not_available'))
