1cf98e198ebc768a6195aa77b47fc1dcfef07c2e
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2022-05-10 11:54:09 +0100

    Add V2 prosecution_case page call to controller
    Add hearing_summaries activeresource request to the prosecution case controller
    Add the decorator_class argument for the specialised CdApi/V2 ProecutionCase decorator
    
    Use the param[:id] as the urn to build the breadcrumb link to the prosecution case page (This is until we can get the URN back as part of the hearing
    summaries endpoint call v2)
    
    Update the search model to take in a version2 param, which decides when to use the version 2 defendants_search. This decouples the Search model from
    the version call. Update the version 2 argument to be from defendants search on the searches contorller and hearing_summaries on the prosecution case controller

--- app/controllers/prosecution_cases_controller.rb
+++ app/controllers/prosecution_cases_controller.rb
@@ -14,17 +14,59 @@ class ProsecutionCasesController < ApplicationController
   private
 
   def load_and_authorize_search
-    @search = Search.new(filter: 'case_reference', term: params[:id])
-    authorize! :create, @search
+    if version_2?
+      authorize! :read, CdApi::ProsecutionCase
+    else
+      @search = Search.new(filter: 'case_reference', term: params[:id], version2: version_2?)
+      authorize! :create, @search
+    end
   end
 
   def set_prosecution_case
-    @prosecution_case = helpers.decorate(search_results.first)
+    build_prosecution_case
+  rescue ActiveResource::BadRequest
+    logger.info 'CLIENT_ERROR_OCCURRED'
+    redirect_to_search_path
+  rescue ActiveResource::ServerError, ActiveResource::ClientError => e
+    logger.error 'SERVER_ERROR_OCCURRED'
+    Sentry.capture_exception(e)
+    redirect_to_search_path
+  end
+
+  def build_prosecution_case
+    @prosecution_case ||= if version_2?
+                            helpers.decorate(search_results, CdApi::ProsecutionCaseDecorator)
+                          else
+                            helpers.decorate(search_results.first)
+                          end
+    update_prosecution_case
+  end
+
+  def update_prosecution_case
+    @prosecution_case.prosecution_case_reference = urn if version_2?
     @prosecution_case.hearings_sort_column = params[:column]
     @prosecution_case.hearings_sort_direction = params[:direction]
   end
 
   def search_results
-    @search_results ||= @search.execute
+    @search_results ||= version_2? ? query_cd_api : @search.execute
+  end
+
+  def version_2?
+    Feature.enabled?(:hearing_summaries)
+  end
+
+  def urn
+    params[:id]
+  end
+
+  def query_cd_api
+    logger.info 'V2_PROSECUTION_CASE'
+    CdApi::ProsecutionCase.find(urn)
+  end
+
+  def redirect_to_search_path
+    redirect_to searches_path(search: { filter: 'case_reference', term: urn })
+    flash[:notice] = I18n.t('prosecution_case.show.failure')
   end
 end
