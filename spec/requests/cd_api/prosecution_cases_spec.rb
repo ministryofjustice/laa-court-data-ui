2203e15ede3780914e65eddec869b30a33e5f85d
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2022-05-10 11:59:08 +0100

    Add request test for V2 prosecution case page
    Add tests for rendering the prosecution_case/show page
    Add tests for exception handling. This sends the user to the search results page - TechDebt: it may change to a more convenient solution for the user
    
    Also, clean up code on the spec/requests/linking/link_defendant_maat_reference_v2_spec.rb

---
+++ spec/requests/cd_api/prosecution_cases_spec.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+RSpec.describe 'prosecution cases', type: :request, stub_case_search: true,
+                                    stub_hearing_summaries_v2: true do
+  let(:user) { create(:user) }
+  let(:case_urn) { 'THISISSTUBBED' }
+
+  context 'when authenticated' do
+    before do
+      sign_in user
+      allow(Feature).to receive(:enabled?).with(:hearing_summaries).and_return(true)
+      allow(Feature).to receive(:enabled?).with(:defendants_search).and_return(false)
+      get "/prosecution_cases/#{case_urn}"
+    end
+
+    it 'renders prosecution_cases/show' do
+      expect(response).to render_template('prosecution_cases/show')
+    end
+
+    context 'when defendants_search flag is on', stub_defendants_case_search: true do
+      before { allow(Feature).to receive(:enabled?).with(:defendants_search).and_return(true) }
+
+      it 'renders prosecution_cases/show' do
+        expect(response).to render_template('prosecution_cases/show')
+      end
+
+      context 'when exception ActiveResource::BadRequest is raised' do
+        before do
+          allow(CdApi::ProsecutionCase).to receive(:find).and_raise(ActiveResource::BadRequest, 'Fake error')
+          get "/prosecution_cases/#{case_urn}"
+        end
+
+        let(:search_params) do
+          { search: { term: case_urn, filter: :case_reference } }
+        end
+
+        it 'redirects to the searches page' do
+          expect(response).to redirect_to(searches_path(search_params))
+        end
+      end
+
+      context 'when exception ActiveResource::ServerError is raised' do
+        before do
+          allow(CdApi::ProsecutionCase).to receive(:find).and_raise(ActiveResource::ServerError, 'Fake error')
+        end
+
+        let(:search_params) do
+          { search: { term: case_urn, filter: :case_reference } }
+        end
+
+        it 'redirects to the searches page' do
+          get "/prosecution_cases/#{case_urn}"
+          expect(response).to redirect_to(searches_path(search_params))
+        end
+
+        it 'captures the exception in Sentry' do
+          allow(Sentry).to receive(:capture_exception).with(ActiveResource::ServerError)
+          get "/prosecution_cases/#{case_urn}"
+          expect(Sentry).to have_received(:capture_exception)
+        end
+      end
+    end
+  end
+
+  context 'when not authenticated' do
+    before do
+      get "/prosecution_cases/#{case_urn}"
+    end
+
+    it 'redirects to sign in page' do
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+end
