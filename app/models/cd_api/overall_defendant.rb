3d4bf5f1e2e88d28562e62258b75fd8050c4fe59
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2022-05-11 10:04:09 +0100

    Remove attr_accessor for v2 defendant

--- app/models/cd_api/overall_defendant.rb
+++ app/models/cd_api/overall_defendant.rb
@@ -2,8 +2,6 @@
 
 module CdApi
   class OverallDefendant < BaseModel
-    attr_accessor :hearing_type
-
     belongs_to :case_summary
     has_many :hearing_days, class_name: 'cd_api/hearing_day'
   end
