386e2dc0375b2a07211a4aead1a03f662dd5a200
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2022-05-13 16:59:51 +0100

    removed logger tests

--- spec/services/cd_api/case_summary_service_spec.rb
+++ spec/services/cd_api/case_summary_service_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe CdApi::CaseSummaryService do
       subject(:case_service) { described_class.call(params:) }
 
       let(:case_reference) { 'TEST12345' }
-      let(:params) { {:urn => case_reference} }
+      let(:params) { { urn: case_reference } }
 
       context 'when successful response', stub_v2_hearing_summary: true, vcr: true do
         it 'returns response with prosecution case reference' do
@@ -15,43 +15,14 @@ RSpec.describe CdApi::CaseSummaryService do
 
         it 'returns response with hearing summaries' do
           expect(case_service.hearing_summaries.size).to eq 3
-        end 
+        end
 
         it 'returns response with overall defendants' do
           expect(case_service.overall_defendants.size).to eq 4
         end
 
-
         it 'returns response mapped to CdApi::CaseSummary' do
           expect(case_service).to be_instance_of(CdApi::CaseSummary)
-        end
-      end
-
-      context 'when invalid endpoint call' do
-        before do
-          stub_request(:get, %r{/v2/hearingsummaries/#{case_reference}}).to_return(status: 400, body: '',
-                                                           headers: { 'Content-Type' => 'application/json' })
-        end
-
-        it 'rescues ActiveResource::BadRequest exception' do
-          allow(Rails.logger).to receive(:info)
-          expect(Rails.logger).to receive(:info).with('CLIENT_ERROR_OCCURRED')
-
-          case_service
-        end
-      end
-
-      context 'when server error' do
-        before do
-          stub_request(:get, %r{/v2/hearingsummaries/#{case_reference}}).to_return(status: 500, body: '',
-                                                           headers: { 'Content-Type' => 'application/json' })
-        end
-
-        it 'rescues ActiveResource::ServerError exception' do
-          allow(Rails.logger).to receive(:error)
-          expect(Rails.logger).to receive(:error ).with('SERVER_ERROR_OCCURRED')
-
-          case_service
         end
       end
     end
