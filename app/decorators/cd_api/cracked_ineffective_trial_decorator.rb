9b2a4ebbc3debefd16a2071c7ae332b9415b843e
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2022-06-01 16:55:10 +0100

    Remove cracked? from CdApi::cracked_ineffective_trial_decorator removed the cracked? method from CdApi::cracked_ineffective_trial_decorator as it is not needed for now (not doing v2 on prosecution_case page yet)

--- app/decorators/cd_api/cracked_ineffective_trial_decorator.rb
+++ app/decorators/cd_api/cracked_ineffective_trial_decorator.rb
@@ -2,17 +2,6 @@
 
 module CdApi
   class CrackedIneffectiveTrialDecorator < BaseDecorator
-    VACATED_CRACKED_TRIAL_CODES = %w[A L M N O Q].freeze
-
-    def cracked?
-      return unless type
-
-      [
-        type.casecmp('cracked').zero?,
-        type.casecmp('vacated').zero? && code.in?(VACATED_CRACKED_TRIAL_CODES)
-      ].any?
-    end
-
     def cracked_on_sentence(hearing)
       t('cracked_ineffective_trial.cracked_on_sentence',
         type: type&.humanize,
