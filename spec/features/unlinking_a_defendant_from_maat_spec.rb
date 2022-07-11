cabac857db2dde997c6faf8ffd68f02f2ce60ac4
SamKyprianou <samuel.kyprianou@digital.justice.gov.uk>
2022-07-11 11:46:57 +0100

    remove V1 MAAT spec file

--- spec/features/unlinking_a_defendant_from_maat_spec.rb
+++ spec/features/unlinking_a_defendant_from_maat_spec.rb
@@ -2,13 +2,16 @@
 
 require 'court_data_adaptor'
 
-RSpec.feature 'Unlinking a defendant from MAAT', type: :feature do
-  let(:defendant_nino_from_fixture) { 'JC123456A' }
-  let(:case_reference_from_fixture) { 'TEST12345' }
+RSpec.feature 'Unlinking a defendant from MAAT', type: :feature, stub_unlink_v2: true do
+  let(:case_urn) { 'TEST12345' }
+  let(:api_url_v2) { CdApi::BaseModel.site }
+  let(:api_request_path) { "#{api_url_v2}laa_references/#{defendant_id}/" }
 
   let(:user) { create(:user) }
 
   before do
+    allow(Feature).to receive(:enabled?).with(:defendants_page).and_return(false)
+    allow(Feature).to receive(:enabled?).with(:laa_references).and_return(true)
     sign_in user
 
     create(:unlink_reason,
@@ -17,26 +20,12 @@ RSpec.feature 'Unlinking a defendant from MAAT', type: :feature do
            text_required: false)
     create(:unlink_reason, code: 7, description: 'Other', text_required: true)
 
-    query = hash_including({ filter: { national_insurance_number: defendant_nino_from_fixture } })
-    body = load_json_stub(defendant_fixture)
-    defendant_body = load_json_stub(defendant_by_id_fixture)
-    json_api_header = { 'Content-Type' => 'application/vnd.api+json' }
-
-    stub_request(:get, "#{api_url}/prosecution_cases")
-      .with(query:)
-      .to_return(body:, headers: json_api_header)
-
-    stub_request(:get, "#{api_url}/defendants/#{defendant_id_from_fixture}?include=offences")
-      .to_return(body: defendant_body, headers: json_api_header)
-
     visit(url)
   end
 
   context 'when user views unlinked defendant' do
-    let(:defendant_fixture) { 'unlinked/defendant_by_reference_body.json' }
-    let(:defendant_by_id_fixture) { 'unlinked_defendant.json' }
-    let(:defendant_id_from_fixture) { '41fcb1cd-516e-438e-887a-5987d92ef90f' }
-    let(:url) { "laa_references/new?id=#{defendant_id_from_fixture}&urn=#{case_reference_from_fixture}" }
+    let(:defendant_id) { '41fcb1cd-516e-438e-887a-5987d92ef90f' }
+    let(:url) { "laa_references/new?id=#{defendant_id}&urn=#{case_urn}" }
 
     it 'displays the MAAT ID field' do
       expect(page).to have_field('MAAT ID')
@@ -56,11 +45,9 @@ RSpec.feature 'Unlinking a defendant from MAAT', type: :feature do
   end
 
   context 'when user views linked defendant' do
-    let(:defendant_fixture) { 'linked/defendant_by_reference_body.json' }
-    let(:defendant_by_id_fixture) { 'linked_defendant.json' }
-    let(:defendant_id_from_fixture) { '41fcb1cd-516e-438e-887a-5987d92ef90f' }
-    let(:url) { "defendants/#{defendant_id_from_fixture}/edit?urn=#{case_reference_from_fixture}" }
-    let(:path) { "#{api_url}/defendants/#{defendant_id_from_fixture}" }
+    let(:defendant_id) { '41fcb1cd-516e-438e-887a-5987d92ef90f' }
+    let(:url) { "defendants/#{defendant_id}/edit?urn=#{case_urn}" }
+    let(:maat_reference) { 2_123_456 }
 
     it 'does not display the MAAT ID field' do
       expect(page).not_to have_field('MAAT ID')
@@ -79,26 +66,12 @@ RSpec.feature 'Unlinking a defendant from MAAT', type: :feature do
     end
 
     context 'when user unlinks with success' do
-      before do
-        stub_request(:patch, path)
-          .to_return(
-            status: 202,
-            body: '',
-            headers: { 'Content-Type' => 'text/plain; charset=utf-8' }
-          )
-      end
-
-      let(:adaptor_request_payload) do
+      let(:api_request_payload) do
         {
-          data:
-          {
-            id: defendant_id_from_fixture,
-            type: 'defendants',
-            attributes: {
-              user_name: user.username,
-              unlink_reason_code: 1
-            }
-          }
+          defendant_id:,
+          user_name: user.username,
+          unlink_reason_code: 1,
+          maat_reference:
         }
       end
 
@@ -109,9 +82,9 @@ RSpec.feature 'Unlinking a defendant from MAAT', type: :feature do
           click_button 'Remove link to court data'
         end
 
-        it 'sends an unlink request to the adapter' do
-          expect(a_request(:patch, path)
-            .with(body: adaptor_request_payload.to_json))
+        it 'sends an unlink request to CD API' do
+          expect(a_request(:patch, api_request_path)
+            .with(body: api_request_payload.to_json))
             .to have_been_made
         end
 
@@ -122,18 +95,13 @@ RSpec.feature 'Unlinking a defendant from MAAT', type: :feature do
       end
 
       context 'with other reason' do
-        let(:adaptor_request_payload) do
+        let(:api_request_payload) do
           {
-            data:
-            {
-              id: defendant_id_from_fixture,
-              type: 'defendants',
-              attributes: {
-                user_name: user.username,
-                unlink_reason_code: 7,
-                unlink_other_reason_text: 'Case already concluded'
-              }
-            }
+            defendant_id:,
+            user_name: user.username,
+            unlink_reason_code: 7,
+            maat_reference:,
+            unlink_other_reason_text: 'Case already concluded'
           }
         end
 
@@ -144,9 +112,9 @@ RSpec.feature 'Unlinking a defendant from MAAT', type: :feature do
           click_button 'Remove link to court data'
         end
 
-        it 'sends an unlink request to the adapter' do
-          expect(a_request(:patch, path)
-            .with(body: adaptor_request_payload.to_json))
+        it 'sends an unlink request to CD API' do
+          expect(a_request(:patch, api_request_path)
+            .with(body: api_request_payload.to_json))
             .to have_been_made
         end
 
@@ -157,15 +125,8 @@ RSpec.feature 'Unlinking a defendant from MAAT', type: :feature do
       end
     end
 
-    context 'when user unlinks defendant with failure' do
+    context 'when user unlinks defendant with failure', stub_v2_unlink_bad_response: true do
       before do
-        stub_request(:patch, path)
-          .to_return(
-            status: 400,
-            body: '{"user_name":["must not exceed 10 characters"]}',
-            headers: { 'Content-Type' => 'application/vnd.api+json; charset=utf-8' }
-          )
-
         click_govuk_detail_summary 'Remove link to court data'
         select 'Linked to wrong case ID (correct defendant)', from: 'Reason for unlinking'
         click_button 'Remove link to court data'
@@ -175,15 +136,13 @@ RSpec.feature 'Unlinking a defendant from MAAT', type: :feature do
         expect(page).to \
           have_govuk_flash(
             :alert,
-            text: /The link to the court data source could not be removed\./
+            text: 'Unable to unlink this defendant'
           )
-      end
 
-      it 'flashes returned error' do
         expect(page).to \
           have_govuk_flash(
             :alert,
-            text: /User name must not exceed 10 characters/
+            text: 'User name must not exceed 10 characters'
           )
       end
     end
