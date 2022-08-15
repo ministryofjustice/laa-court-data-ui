e66fafe0dc522baa0e4eacf365910bd632c8064b
Obsiye <Obsiye@users.noreply.github.com>
2022-08-15 16:16:30 +0100

    Enable defence counsel to defendent relationship
    Map the defence_counsel.defendants (defendant ids) to the hearing_summary.defendants (objects) in the HearingSumamries Decorator.
    This allows for the defence counsel summary decorator to easily build a name_status_and_defendant list e.g. John Doe (QC) for Defendant ABC
    
    Add duplicated defence counsels when there are duplicated defendants in DefenceCounselDecorator e.g.
    John Doe (QC) for Defendant A
    John Doe (QC) for Defendant B
    which is preferred over "John Doe (QC) for Defendant A and Defendant B" because of clarity on the case page.

--- app/decorators/cd_api/defence_counsel_decorator.rb
+++ app/decorators/cd_api/defence_counsel_decorator.rb
@@ -2,10 +2,16 @@
 
 module CdApi
   class DefenceCounselDecorator < BaseDecorator
-    def name_and_status
+
+    def name_status_and_defendants
       return t('generic.not_available') if name_and_status_blank?
 
-      "#{formatted_name} (#{formatted_status})"
+      return "#{formatted_name} (#{formatted_status})" if defendants.empty?
+
+      defence_counsel_list = formatted_multiples_defendant_names.map do |defendant_name|
+        "#{formatted_name} (#{formatted_status}) for #{defendant_name}"
+      end
+      safe_join(defence_counsel_list, tag.br)
     end
 
     private
@@ -28,6 +34,25 @@ module CdApi
 
     def formatted_status
       status || t('generic.not_available').downcase
+    end
+
+    def formatted_multiples_defendant_names
+      names = []
+      defendants.map do |defendant|
+        names << format_defendant_name(defendant&.first_name, defendant&.middle_name, defendant&.last_name)
+      end
+
+      return [] if names.empty?
+
+      names.compact
+    end
+
+    def format_defendant_name(first_name, middle_name, last_name)
+      # TODO: Create a name service to build the name for reusability
+      # and confirm the must have portions of a name or validations needed
+      return nil unless first_name || middle_name || last_name
+
+      [first_name, middle_name, last_name].compact.reject(&:empty?).join(' ')
     end
   end
 end
