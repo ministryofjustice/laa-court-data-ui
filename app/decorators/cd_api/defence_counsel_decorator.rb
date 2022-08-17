247fd170af3f2126fe79e12c68877689541d4d95
Obsiye <Obsiye@users.noreply.github.com>
2022-08-17 10:02:14 +0100

    Remove TODO techdebt for name building
    Remove this todo to create a service to build a fullname out of parts: firstname, middlename, lastname.
    
    This is because a ticket was creatd to work on this https://dsdmoj.atlassian.net/browse/AAC-808

--- app/decorators/cd_api/defence_counsel_decorator.rb
+++ app/decorators/cd_api/defence_counsel_decorator.rb
@@ -20,8 +20,6 @@ module CdApi
     end
 
     def name
-      # TODO: Create a name service to build the name for reusability
-      # and confirm the must have portions of a name or validations needed
       return nil unless first_name || middle_name || last_name
 
       [first_name, middle_name, last_name].compact.reject(&:empty?).join(' ')
@@ -45,8 +43,6 @@ module CdApi
     end
 
     def format_defendant_name(first_name, middle_name, last_name)
-      # TODO: Create a name service to build the name for reusability
-      # and confirm the must have portions of a name or validations needed
       return nil unless first_name || middle_name || last_name
 
       [first_name, middle_name, last_name].compact.reject(&:empty?).join(' ')
