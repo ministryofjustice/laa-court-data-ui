3a7fd27754dead94849c8cf321b5f06720b6954d
Obsiye <Obsiye@users.noreply.github.com>
2022-08-24 09:58:54 +0100

    Update the hearing decorator to list out defence counsels
    Add a defence_counsels_list public method to create a list of defence counsels e.g. John Smith (QC) for Jane Doe
    
    In order to create the above list, we have to first map the defendants in the defence_counsels.defendants (which is a list of ids) to the
    defendant details in the prosecution_cases under the hearing object.
    
    Call the DefenceCounselsListService to build the list of names using a name builder logic as we get parts of a fullname e.g. first_name, last_name
    
    Add map_defence_counsels to map the defendant_ids to the defendant details. It searches through each prosecution case that the hearing is linked to in order to find
    the details of the defendant.

--- app/decorators/cd_api/hearing_decorator.rb
+++ app/decorators/cd_api/hearing_decorator.rb
@@ -6,5 +6,40 @@ module CdApi
       @cracked_ineffective_trial ||= decorate(object.hearing.cracked_ineffective_trial,
                                               CdApi::CrackedIneffectiveTrialDecorator)
     end
+
+    def defence_counsels_list
+      # TODO: Add logic of attendance days for each defence
+      return t('generic.not_available') if hearing.defence_counsels.blank?
+
+      safe_join(defence_counsel_sentences, tag.br)
+    end
+
+    private
+
+    def defence_counsel_sentences
+      decorated_defence_counsels || []
+    end
+
+    def decorated_defence_counsels
+      CdApi::Hearing::DefenceCounselsListService.call(mapped_defence_counsels)
+    end
+
+    def mapped_defence_counsels
+      @mapped_defence_counsels ||= map_defence_counsels
+    end
+
+    def map_defence_counsels
+      hearing.prosecution_cases.each do |pc|
+        # TODO: Confirm that defendant details is the same across cases and searching cases for a defence_counsel.defendant.id is accurate
+        hearing.defence_counsels.each do |defence_counsel|
+          defence_counsel.defendants.map! do |dc_defendant_id|
+            next dc_defendant_id unless dc_defendant_id.is_a? String
+
+            pc.defendants.find { |defendant| (dc_defendant_id == defendant.id) }
+          end
+        end
+      end
+      hearing.defence_counsels
+    end
   end
 end
