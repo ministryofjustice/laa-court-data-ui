6294c4b447407be6aad193c22d7bfd9dab8e86b7
Obsiye <Obsiye@users.noreply.github.com>
2022-08-24 11:54:12 +0100

    Add factories for hearing object
    Add factories for unit testing the hearing object.
    
    Add a factory for hearing and hearing_details: hearing object has an attribute hearing within it so to prevent duplciation hearing_details factory name was created
    
    Update the cracked_ineffective_trial_decorator to make use of the new hearing factories.

---
+++ spec/factories/cd_api/hearing/defendant.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  # ActiveResource Factory, use :build not :create to prevent HTTP calls
+  factory :hearing_defendant, class: 'CdApi::Defendant' do
+    id { SecureRandom.uuid }
+    prosecution_case_id { '' }
+    offences { [] }
+    defence_organisation { {} }
+    defendant_details { {} }
+    judicial_results { [] }
+    legal_aid_status { nil }
+    proceedings_concluded { nil }
+    isYouth { nil }
+
+    trait :with_defendant_details do
+      defendant_details { build :hearing_defendant_details, :with_person_details }
+    end
+  end
+end
