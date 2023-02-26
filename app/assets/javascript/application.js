ee4cc4752834288e4c41007757d1e656cd113e3e
Paul King <paul.king@digital.justice.gov.uk>
2023-02-26 20:27:23 +0000

    Cleanup existing files

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -6,8 +6,6 @@
 import '../stylesheets/application.scss'
 import * as Turbo from "@hotwired/turbo"
 require('@rails/ujs').start()
-// require("@rails/activestorage").start();
-// require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/webpack/packs/govuk-frontend.js
+++
@@ -1,6 +0,0 @@
-import { initAll } from 'govuk-frontend'
-
-document.addEventListener('turbo:load', function (event) {
-  require.context('govuk-frontend/govuk/assets')
-  initAll()
-})
