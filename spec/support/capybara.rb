8e4f4ac392d79a05a7d15b4103776bf2a43f86f7
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2023-10-20 13:50:52 +0100

    Use browser for selenium tests

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -34,5 +34,5 @@ Capybara.configure do |config|
   # https://www.rubydoc.info/github/jnicklas/capybara/Capybara.configure
   config.automatic_label_click = true
   config.default_max_wait_time = 1
-  config.javascript_driver = :selenium_chrome_headless
+  config.javascript_driver = :selenium_chrome
 end
