cea832d7911807b17e93ffb70cf13abe2884e597
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2023-10-21 21:48:05 +0100

    less flaky failures

--- spec/features/users/delete_user_spec.rb
+++ spec/features/users/delete_user_spec.rb
@@ -15,13 +15,12 @@ RSpec.feature 'Delete user', :js, type: :feature do
       row = page.find(%(tr[data-user-id="#{other_user.id}"]))
 
       within row do
-        click_link 'Delete'
+        find_link('Delete').click
       end
 
       warning = page.driver.browser.switch_to.alert
       expect(warning.text).to eql "Are you sure you want to delete #{other_user.name}'s account?"
       warning.accept
-
       expect(page).to have_current_path(users_path)
       expect(page).to have_govuk_flash(:notice, text: 'User successfully deleted')
     end
