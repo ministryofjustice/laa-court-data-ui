9fb3f44169e7f7af9ab6adec2ffbf6e107fc95b1
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2024-07-05 15:58:48 +0100

    Dependabot updates 05/07/2024 (#2120)
    * Bump postcss from 8.4.38 to 8.4.39
    
    Bumps [postcss](https://github.com/postcss/postcss) from 8.4.38 to 8.4.39.
    - [Release notes](https://github.com/postcss/postcss/releases)
    - [Changelog](https://github.com/postcss/postcss/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/postcss/postcss/compare/8.4.38...8.4.39)
    
    ---
    updated-dependencies:
    - dependency-name: postcss
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump cypress from 13.12.0 to 13.13.0
    
    Bumps [cypress](https://github.com/cypress-io/cypress) from 13.12.0 to 13.13.0.
    - [Release notes](https://github.com/cypress-io/cypress/releases)
    - [Changelog](https://github.com/cypress-io/cypress/blob/develop/CHANGELOG.md)
    - [Commits](https://github.com/cypress-io/cypress/compare/v13.12.0...v13.13.0)
    
    ---
    updated-dependencies:
    - dependency-name: cypress
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump sidekiq from 7.2.4 to 7.3.0
    
    Bumps [sidekiq](https://github.com/sidekiq/sidekiq) from 7.2.4 to 7.3.0.
    - [Changelog](https://github.com/sidekiq/sidekiq/blob/main/Changes.md)
    - [Commits](https://github.com/sidekiq/sidekiq/compare/v7.2.4...v7.3.0)
    
    ---
    updated-dependencies:
    - dependency-name: sidekiq
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump the rubocop group across 1 directory with 2 updates
    
    Bumps the rubocop group with 2 updates in the / directory: [rubocop-rails](https://github.com/rubocop/rubocop-rails) and [rubocop-rspec](https://github.com/rubocop/rubocop-rspec).
    
    Updates `rubocop-rails` from 2.25.0 to 2.25.1
    - [Release notes](https://github.com/rubocop/rubocop-rails/releases)
    - [Changelog](https://github.com/rubocop/rubocop-rails/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/rubocop/rubocop-rails/compare/v2.25.0...v2.25.1)
    
    Updates `rubocop-rspec` from 3.0.1 to 3.0.2
    - [Release notes](https://github.com/rubocop/rubocop-rspec/releases)
    - [Changelog](https://github.com/rubocop/rubocop-rspec/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/rubocop/rubocop-rspec/compare/v3.0.1...v3.0.2)
    
    ---
    updated-dependencies:
    - dependency-name: rubocop-rails
      dependency-type: direct:development
      update-type: version-update:semver-patch
      dependency-group: rubocop
    - dependency-name: rubocop-rspec
      dependency-type: direct:development
      update-type: version-update:semver-patch
      dependency-group: rubocop
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Fix RSpec/PredicateMatcher offences
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- spec/lib/court_data_adaptor/query/defendant/reference_parser_spec.rb
+++ spec/lib/court_data_adaptor/query/defendant/reference_parser_spec.rb
@@ -57,7 +57,7 @@ RSpec.describe CourtDataAdaptor::Query::Defendant::ReferenceParser do
 
       terms.each do |term, message|
         it "#{term} - #{message}" do
-          expect(described_class.new(term).national_insurance_number?).to be_truthy, "expected #{term} to be considered a valid national insurance number because \"#{message}\""
+          expect(described_class.new(term)).to be_national_insurance_number, "expected #{term} to be considered a valid national insurance number because \"#{message}\""
         end
       end
     end
@@ -84,7 +84,7 @@ RSpec.describe CourtDataAdaptor::Query::Defendant::ReferenceParser do
 
       terms.each do |term, message|
         it "#{term} - #{message}" do
-          expect(described_class.new(term).national_insurance_number?).to be_falsey, "expected #{term} to be considered an invalid national insurance number because\"#{message}\""
+          expect(described_class.new(term)).not_to be_national_insurance_number, "expected #{term} to be considered an invalid national insurance number because\"#{message}\""
         end
       end
     end
