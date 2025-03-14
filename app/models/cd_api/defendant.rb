63eece309b9887d6400bf6b8d8eb53b9e6ac1f55
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-03-14 12:42:04 +0000

    Fix typo

--- app/models/cd_api/defendant.rb
+++ app/models/cd_api/defendant.rb
@@ -2,7 +2,7 @@
 
 module CdApi
   class Defendant < BaseModel
-    has_many :offence_summary, class_name: 'cd_api/offence_summary'
+    has_many :offence_summaries, class_name: 'cd_api/offence_summary'
 
     def linked?
       maat_references.first.present? && maat_references.first.first != 'Z'
