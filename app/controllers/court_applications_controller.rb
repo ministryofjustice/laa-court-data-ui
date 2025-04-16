d96c820d77f69bdffc87140f0407da33e9c411d0
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-04-16 14:44:57 +0100

    Address review comments

--- app/controllers/court_applications_controller.rb
+++ app/controllers/court_applications_controller.rb
@@ -1,4 +1,6 @@
 class CourtApplicationsController < ApplicationController
+  DEFAULT_SORT_DIRECTION = "asc".freeze
+
   before_action :load_and_authorize_application
 
   add_breadcrumb :search_filter_breadcrumb_name, :new_search_filter_path
@@ -6,7 +8,7 @@ class CourtApplicationsController < ApplicationController
 
   def show
     add_breadcrumb @application.application_title
-    @date_sort_direction = params.fetch(:date_sort_direction, "asc")
+    @date_sort_direction = params.fetch(:date_sort_direction, DEFAULT_SORT_DIRECTION)
   end
 
   private
@@ -15,6 +17,7 @@ class CourtApplicationsController < ApplicationController
     @application = CourtDataAdaptor::Query::CourtApplication.new(params[:id]).call
     authorize! :show, @application
   rescue JsonApiClient::Errors::ServiceUnavailable => e
+    logger.error "COURT APPLICATION COULD NOT BE RETRIEVED: #{e.message}"
     Sentry.capture_exception(e)
     redirect_to controller: :errors, action: :internal_error
   rescue JsonApiClient::Errors::NotFound
