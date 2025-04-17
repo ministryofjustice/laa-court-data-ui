f052f8687fa1456dee4f0215defffa959fa31f72
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2025-04-17 11:50:20 +0100

    Mechanism to unlink court applications (#2902)

---
+++ lib/court_data_adaptor/query/unlink_court_application.rb
@@ -0,0 +1,33 @@
+module CourtDataAdaptor
+  module Query
+    class UnlinkCourtApplication < Base
+      acts_as_resource CourtDataAdaptor::Resource::ApplicationSummary
+
+      alias unlink_object term
+
+      def call
+        refresh_token_if_required!
+
+        response = resource.connection.faraday.patch(
+          "court_application_laa_references/#{unlink_object.defendant_id}",
+          unlink_params
+        )
+
+        response.status.in?(200..299)
+      end
+
+      private
+
+      def unlink_params
+        {
+          laa_reference: {
+            subject_id: unlink_object.defendant_id,
+            user_name: unlink_object.username,
+            unlink_reason_code: unlink_object.reason_code,
+            unlink_other_reason_text: unlink_object.other_reason_text
+          }
+        }
+      end
+    end
+  end
+end
