84a19ed0d340c07742af4aa0e7ce90addbc4ff3b
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-05-06 15:07:32 +0100

    Use correct tag in different circumstances

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,9 +2,27 @@
 
 ENVIRONMENT=$1
 
-helm upgrade laa-court-data-ui ./helm_deploy/. \
-  --install --wait \
-  --namespace=${K8S_NAMESPACE} \
-  --values ./helm_deploy/values/$ENVIRONMENT.yaml \
-  --set image.repository="754256621582.dkr.ecr.${ECR_REGION}.amazonaws.com/laa-assess-a-claim/laa-court-data-ui" \
-  --set image.tag="main-$CIRCLE_SHA1"
+deploy_branch() {
+  # TODO: Set up ephemeral deployment here
+  helm upgrade laa-court-data-ui ./helm_deploy/. \
+    --install --wait \
+    --namespace=${K8S_NAMESPACE} \
+    --values ./helm_deploy/values/$ENVIRONMENT.yaml \
+    --set image.repository="754256621582.dkr.ecr.${ECR_REGION}.amazonaws.com/laa-assess-a-claim/laa-court-data-ui" \
+    --set image.tag="branch-$CIRCLE_SHA1"
+}
+
+deploy_main() {
+  helm upgrade laa-court-data-ui ./helm_deploy/. \
+    --install --wait \
+    --namespace=${K8S_NAMESPACE} \
+    --values ./helm_deploy/values/$ENVIRONMENT.yaml \
+    --set image.repository="754256621582.dkr.ecr.${ECR_REGION}.amazonaws.com/laa-assess-a-claim/laa-court-data-ui" \
+    --set image.tag="main-$CIRCLE_SHA1"
+}
+
+if [[ "$CIRCLE_BRANCH" == "main" ]]; then
+  deploy_main
+else
+  deploy_branch
+fi
