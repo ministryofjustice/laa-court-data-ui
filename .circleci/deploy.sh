23a8a1c9e0a5d5e4683edc393cc09b27309a7eb5
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-05-06 14:20:11 +0100

    Stop using some missing vars

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -6,5 +6,5 @@ helm upgrade laa-court-data-ui ./helm_deploy/. \
   --install --wait \
   --namespace=${K8S_NAMESPACE} \
   --values ./helm_deploy/values/$ENVIRONMENT.yaml \
-  --set image.repository="${AWS_ECR_REGISTRY_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/${ECR_REPOSITORY}" \
+  --set image.repository="754256621582.dkr.ecr.${ECR_REGION}.amazonaws.com/${ECR_REPOSITORY}" \
   --set image.tag="main-$CIRCLE_SHA1"
