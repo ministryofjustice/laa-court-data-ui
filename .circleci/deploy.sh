3ce662a04265c98234243e4d7f0772dedc7a9eac
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-05-06 14:59:00 +0100

    Is it the wrong team name?

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -6,5 +6,5 @@ helm upgrade laa-court-data-ui ./helm_deploy/. \
   --install --wait \
   --namespace=${K8S_NAMESPACE} \
   --values ./helm_deploy/values/$ENVIRONMENT.yaml \
-  --set image.repository="754256621582.dkr.ecr.${ECR_REGION}.amazonaws.com/${ECR_REPOSITORY}" \
+  --set image.repository="754256621582.dkr.ecr.${ECR_REGION}.amazonaws.com/laa-assess-a-claim/laa-court-data-ui" \
   --set image.tag="main-$CIRCLE_SHA1"
