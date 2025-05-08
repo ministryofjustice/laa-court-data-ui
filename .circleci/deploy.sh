7742363f508d28b3b9b9b75a470b97162b4841ae
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2025-05-08 09:48:51 +0100

    ACD-689: Bespoke dev url (#2921)
    * Naive attempt no 1
    
    * Fix copy-paste error
    
    * Fix syntax
    
    * Fix case
    
    * Fix ingress name format and identifier format
    
    * Debug
    
    * Make sure we only deploy to branch on dev
    
    * Show where deployed to
    
    * Remove CSS debug
    
    * Try to automatically delete any helm release on PR close (#2922)

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,14 +2,29 @@
 
 ENVIRONMENT=$1
 
+# Convert the branch name into a string that can be turned into a valid URL
+BRANCH_RELEASE_NAME=$(echo $CIRCLE_BRANCH | tr '[:upper:]' '[:lower:]' | sed 's:^\w*\/::' | tr -s ' _/[]().' '-' | cut -c1-18 | sed 's/-$//')
+
 deploy_branch() {
-  # TODO: Set up ephemeral deployment here
-  helm upgrade laa-court-data-ui ./helm_deploy/. \
+  # Set the deployment host, this will add the prefix of the branch name
+  RELEASE_HOST="$BRANCH_RELEASE_NAME.apps.live.cloud-platform.service.justice.gov.uk"
+  # The identifier is of format <ingress name>-<namespace>-green
+  IDENTIFIER="$BRANCH_RELEASE_NAME-app-ingress-laa-court-data-ui-dev-green"
+
+  helm upgrade $BRANCH_RELEASE_NAME ./helm_deploy/. \
     --install --wait \
     --namespace=${K8S_NAMESPACE} \
     --values ./helm_deploy/values/$ENVIRONMENT.yaml \
     --set image.repository="754256621582.dkr.ecr.${ECR_REGION}.amazonaws.com/laa-assess-a-claim/laa-court-data-ui" \
-    --set image.tag="branch-$CIRCLE_SHA1"
+    --set image.tag="branch-$CIRCLE_SHA1" \
+    --set identifier="$IDENTIFIER" \
+    --set host="$RELEASE_HOST" \
+    --set nameOverride="$BRANCH_RELEASE_NAME"\
+    --set fullnameOverride="$BRANCH_RELEASE_NAME"\
+    --set replicas.web=1\
+    --set branch=true
+
+  echo "DEPLOYED TO: $RELEASE_HOST"
 }
 
 deploy_main() {
@@ -24,5 +39,25 @@ deploy_main() {
 if [[ "$CIRCLE_BRANCH" == "main" ]]; then
   deploy_main
 else
-  deploy_branch
+  if [[ "$K8S_NAMESPACE" == "laa-court-data-ui-dev" ]]; then
+    deploy_branch
+    if [ $? -eq 0 ]; then
+      echo "Deploy succeeded"
+    else
+      # If a previous `helm upgrade` was cancelled this could have got the release stuck in
+      # a "pending-upgrade" state (c.f. https://stackoverflow.com/a/65135726). If so, this
+      # can generally be fixed with a `helm rollback`, so we try that here.
+      echo "Deploy failed. Attempting rollback"
+      helm rollback $BRANCH_RELEASE_NAME
+      if [ $? -eq 0 ]; then
+        echo "Rollback succeeded. Retrying deploy"
+        deploy_branch
+      else
+        echo "Rollback failed. Consider manually running 'helm delete $BRANCH_RELEASE_NAME'"
+        exit 1
+      fi
+    fi
+  else
+    deploy_main
+  fi
 fi
