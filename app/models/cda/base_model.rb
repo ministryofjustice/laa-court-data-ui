86116d8c84faf69044e0e5510deced6a02df7be6
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2025-05-14 16:00:30 +0100

    ACD-678: Load hearing events directly from CDA (#2930)
    * Load hearing events directly from CDA
    
    * Address review comment
    
    * Add missing call to cassette
    
    * Guard against no defendants

---
+++ app/models/cda/base_model.rb
@@ -0,0 +1,12 @@
+module Cda
+  class BaseModel < ActiveResource::Base
+    self.site = CourtDataAdaptor::Resource::V2.api_url
+
+    def self.headers
+      {
+        'Content-Type' => 'application/json',
+        'Authorization' => "Bearer #{CourtDataAdaptor::Client.new.bearer_token}"
+      }
+    end
+  end
+end
