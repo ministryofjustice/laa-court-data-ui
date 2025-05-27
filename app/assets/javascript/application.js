80e2d367cc7d49ef66afdefa7f77ed5becd462cb
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-05-27 14:43:23 +0100

    Disable turbo-forms

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -7,6 +7,14 @@ import '../stylesheets/application.scss'
 import '@hotwired/turbo'
 import Rails from '@rails/ujs'
 
+// Prevent turbo from intercepting form submissions in ways that don't play nicely
+// with things like the Rails flash system. Note:
+// 1) This avoids the need for having to a add a `data-turbo=false` annotation to
+//    individual forms
+// 2) This doesn't disable turbo as a whole, so it will still do link-prefetching,
+//    and the turbo-frame system used for the cookie banner will still work.
+Turbo.config.forms.mode = "off"
+
 Rails.start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
