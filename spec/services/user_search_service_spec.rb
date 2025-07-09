db70a3d056373c01e45432543903cd154457b822
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-07-09 14:39:56 +0100

    Add missing specs

--- spec/services/user_search_service_spec.rb
+++ spec/services/user_search_service_spec.rb
@@ -3,13 +3,18 @@ require "rails_helper"
 RSpec.describe UserSearchService do
   subject(:search_results) { described_class.call(user_search, User).to_a }
 
-  let(:user_search) { UserSearch.new(search_string:) }
+  let(:user_search) { UserSearch.new(search_string:, recent_sign_ins:, old_sign_ins:) }
   let(:searched_user) do
-    create(:user, first_name: 'Jane', last_name: 'Doe', username: 'j-doe', email: 'jane@example.com')
+    create(:user, first_name: 'Jane', last_name: 'Doe', username: 'j-doe', email: 'jane@example.com',
+                  last_sign_in_at: 1.day.ago)
   end
   let(:unsearched_user) do
-    create(:user, first_name: 'John', last_name: 'Smith', username: 'j-smith', email: 'john@example.com')
+    create(:user, first_name: 'John', last_name: 'Smith', username: 'j-smith', email: 'john@example.com',
+                  last_sign_in_at: nil)
   end
+  let(:recent_sign_ins) { false }
+  let(:old_sign_ins) { false }
+  let(:search_string) { nil }
 
   before do
     searched_user
@@ -55,5 +60,24 @@ RSpec.describe UserSearchService do
     let(:search_string) { 'Jane Smith' }
 
     it { is_expected.to be_empty }
+  end
+
+  context 'when filtering by recent sign ins' do
+    let(:recent_sign_ins) { true }
+
+    it { is_expected.to include(searched_user) }
+    it { is_expected.not_to include(unsearched_user) }
+  end
+
+  context 'when filtering by old sign ins' do
+    let(:old_sign_ins) { true }
+
+    before do
+      searched_user.update(last_sign_in_at: 4.months.ago)
+      unsearched_user.update(last_sign_in_at: 1.day.ago)
+    end
+
+    it { is_expected.to include(searched_user) }
+    it { is_expected.not_to include(unsearched_user) }
   end
 end
