cbac4f07334d96f4a6c865a9b9dc8298cae10e89
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2025-07-22 12:15:42 +0100

    Switch to ActiveResource for appeal link/unlink

---
+++ app/models/cda/court_application_laa_reference.rb
@@ -0,0 +1,27 @@
+module Cda
+  class CourtApplicationLaaReference < BaseModel
+    self.element_name = "court_application_laa_reference"
+
+    def self.create(link_attempt)
+      court_application_laa_reference = {
+        maat_reference: link_attempt.maat_reference,
+        subject_id: link_attempt.defendant_id,
+        user_name: link_attempt.username
+      }
+      http_response = post('', nil, { court_application_laa_reference: }.to_json)
+      http_response.code.to_i.in?(200..299)
+    end
+
+    def self.update(unlink_attempt)
+      laa_reference = {
+        subject_id: unlink_attempt.defendant_id,
+        user_name: unlink_attempt.username,
+        unlink_reason_code: unlink_attempt.reason_code,
+        unlink_other_reason_text: (unlink_attempt.other_reason_text if unlink_attempt.text_required?),
+        maat_reference: unlink_attempt.maat_reference
+      }
+      http_response = patch(unlink_attempt.defendant_id, nil, { laa_reference: }.to_json)
+      http_response.code.to_i.in?(200..299)
+    end
+  end
+end
