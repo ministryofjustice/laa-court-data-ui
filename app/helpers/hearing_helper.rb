7bbd3ab4c02707d136e594cdf371f707a7f625fc
Alessandro De Simone <alexdesi@gmail.com>
2025-10-31 11:56:59 +0000

    Fix bug: undefined method 'hearing_days' for an instance of Cda::HearingDecorator

--- app/helpers/hearing_helper.rb
+++ app/helpers/hearing_helper.rb
@@ -5,10 +5,6 @@ module HearingHelper
     HearingPaginator.new(...)
   end
 
-  def earliest_day_for(hearing)
-    hearing.hearing_days.map(&:to_datetime).min
-  end
-
   def transform_and_sanitize(text)
     text_with_converted_crlf = convert_crlf_to_html(text)
     sanitize(text_with_converted_crlf, tags: %w[br p])
