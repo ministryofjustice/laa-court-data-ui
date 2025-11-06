a52c1567355597ce28046c3763a4fb7355f935b8
Deepthi Doppalapudi <deepthi.doppalapudi@justice.gov.uk>
2025-11-06 17:18:45 +0000

    ACD-999 corrected tests as per review comments

--- spec/support/shared_examples_for_search.rb
+++ spec/support/shared_examples_for_search.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'renders search validation errors' do
-  it { expect(response.body).to include('searches/new') }
-  it { expect(response.body).to include('searches/_form') }
-  it { expect(response.body).not_to include('searches/_results') }
+  
   it { expect(response.body).to include('There is a problem') }
 end
 
 RSpec.shared_examples 'renders results' do
-  it { expect(response.body).to include('searches/new') }
-  it { expect(response.body).to include('searches/_form') }
-  it { expect(response.body).to include('searches/_results') }
-  it { expect(response.body).to include('searches/_results_header') }
+  it { expect(response.body).to include('Search for') }
+
 end
