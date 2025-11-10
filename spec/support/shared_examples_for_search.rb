35e669da7bbc096244bff19f6ee6772ffd6b792d
Deepthi Doppalapudi <deepthi.doppalapudi@justice.gov.uk>
2025-11-10 10:01:31 +0000

    ACD-999 removed lint issues

--- spec/support/shared_examples_for_search.rb
+++ spec/support/shared_examples_for_search.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'renders search validation errors' do
-  
   it { expect(response.body).to include('There is a problem') }
 end
 
 RSpec.shared_examples 'renders results' do
   it { expect(response.body).to include('Search for') }
-
 end
