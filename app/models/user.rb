a45762a5bece3a992c9e07c856069b8500a05d37
pijian.liao <pijian.liao@version1.com>
2025-12-15 16:45:54 +0000

    ACD-1112 test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   include Roles
   extend ArrayEnum
 
-  # FEATURE_FLAGS = ["view_appeals"].freeze
-  # array_enum feature_flags: FEATURE_FLAGS.to_h { [it, it] }
+  FEATURE_FLAGS = ["view_appeals"].freeze
+  array_enum feature_flags: FEATURE_FLAGS.to_h { [it, it] }
 
   # Include default devise modules. Others available are:
   # :confirmable, :registerable and :omniauthable
