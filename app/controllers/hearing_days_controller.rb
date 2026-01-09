6d95f6cee51525aff89169c49a2711bed89f9af5
Alessandro De Simone <alexdesi@gmail.com>
2026-01-09 10:46:36 +0000

    Refactor: de-spaghettification of HearingDaysController.
    The logic was spread between many before actions, that were sharing instance variables.
    The show action is longer now, but the logic is centralized and easier to follow.

--- app/controllers/hearing_days_controller.rb
+++ app/controllers/hearing_days_controller.rb
@@ -1,49 +1,23 @@
 class HearingDaysController < ApplicationController
-  before_action :set_application, :load_hearing_days, :load_hearing_details,
-                :load_hearing_events
+  before_action :set_application
 
   add_breadcrumb :search_filter_breadcrumb_name, :new_search_filter_path
   add_breadcrumb :search_breadcrumb_name, :search_breadcrumb_path
-  before_action :add_extra_breadcrumbs
+
+  after_action :add_extra_breadcrumbs
 
   HEARING_SORT_DIRECTION = "asc".freeze
 
   def show
     authorize! :show, @application
-  end
 
-  private
-
-  def set_application
-    @application = Cda::CourtApplication.find(params[:court_application_id])
-  end
-
-  def load_hearing_days
-    load_hearing_day
-    load_adjacent_hearing_days
-  end
-
-  def load_hearing_details
+    @hearing_day = current_hearing_day
+    @previous_day, @next_day = get_adjacent_hearing_days(@hearing_day)
     details = load_hearing
-    @hearing_details = helpers.decorate(details, Cda::HearingDecorator)
-    @hearing_details.current_sitting_day = @hearing_day.sitting_day
-  rescue ActiveResource::ConnectionError => e
-    logger.error e
-    Sentry.capture_exception(e)
-    redirect_to controller: :errors, action: :internal_error
-  end
-
-  def load_hearing
-    Cda::Hearing.find(@hearing_day.hearing.id)
-  rescue ActiveResource::ResourceNotFound
-    # If the hearing has not been resulted, HMCTS will return a 404
-    # We want to handle this gracefully
-    Cda::Hearing.new
-  end
-
-  def load_hearing_events
-    @hearing_events = Cda::HearingEventLog.find_from_hearing_and_date(params[:hearing_id],
-                                                                      @hearing_day.date)
+    @hearing_details = helpers.decorate(details, Cda::HearingDecorator).tap do |decorated_hearing|
+      decorated_hearing.current_sitting_day = @hearing_day.sitting_day
+    end
+    @hearing_events = Cda::HearingEventLog.find_from_hearing_and_date(params[:hearing_id], @hearing_day.date)
   rescue ActiveResource::ResourceNotFound
     logger.warn "No hearing events found for hearing #{params[:hearing_id]}"
   rescue ActiveResource::ConnectionError => e
@@ -52,21 +26,36 @@ class HearingDaysController < ApplicationController
     redirect_to controller: :errors, action: :internal_error
   end
 
-  def load_hearing_day
-    @hearing_day = flat_hearing_days.find do |hearing_day|
+  private
+
+  def set_application
+    @application = Cda::CourtApplication.find(params[:court_application_id])
+  end
+
+  def load_hearing
+    Cda::Hearing.find(params[:hearing_id])
+  rescue ActiveResource::ResourceNotFound
+    # If the hearing has not been resulted, HMCTS will return a 404
+    # We want to handle this gracefully
+    Cda::Hearing.new
+  end
+
+  def current_hearing_day
+    sorted_hearing_days.find do |hearing_day|
       hearing_day.hearing.id == params[:hearing_id] && hearing_day.date == params[:id].to_date
     end
   end
 
-  def load_adjacent_hearing_days
-    current_day_index = flat_hearing_days.index(@hearing_day)
+  def get_adjacent_hearing_days(hearing_day)
+    current_day_index = sorted_hearing_days.index(hearing_day)
 
-    @next_day = flat_hearing_days[current_day_index + 1]
-    @previous_day = flat_hearing_days[current_day_index - 1] if current_day_index.positive?
+    previous_day = sorted_hearing_days[current_day_index - 1] if current_day_index.positive?
+    next_day = sorted_hearing_days[current_day_index + 1]
+    [previous_day, next_day]
   end
 
-  def flat_hearing_days
-    @flat_hearing_days ||= @application.hearing_days_sorted_by(HEARING_SORT_DIRECTION)
+  def sorted_hearing_days
+    @sorted_hearing_days ||= @application.hearing_days_sorted_by(HEARING_SORT_DIRECTION)
   end
 
   def add_extra_breadcrumbs
