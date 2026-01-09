ba04b4764b35df45375e793c78868664b2f55067
pijian.liao <pijian.liao@version1.com>
2026-01-09 16:46:52 +0000

    ACD-1103 fix unit test linter error

--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-
-RSpec.describe Users::SessionsController, :type => :controller do
+RSpec.describe Users::SessionsController, type: :controller do
 
   let(:user) { create(:user, :with_admin_role) }
   let(:other_user) { create(:user, :with_caseworker_role) }
@@ -14,17 +13,14 @@ RSpec.describe Users::SessionsController, :type => :controller do
   end
 
   describe "After caseworker sign-in" do
-
     it "redirects to the search_filters page" do
       expect(controller.after_sign_in_path_for(other_user)).to eq '/search_filters/new'
     end
   end
 
   describe "After data analyst sign-in" do
-
     it "redirects to new_stats_path" do
       expect(controller.after_sign_in_path_for(data_user)).to eq new_stats_path
     end
   end
-
 end
