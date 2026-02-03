8f3cf1284d230e8e8ca824189d2f746571bd947d
pijian.liao <pijian.liao@version1.com>
2026-02-03 14:58:43 +0000

    LAACONNECT-1119 add sort tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,64 +7,62 @@ RSpec.describe UsersController, type: :controller do
     let(:pagy_obj)             { instance_double('Pagy') }
 
     before do
-      # Seed the controller's @users with a relation-like double
       controller.instance_variable_set(:@users, users_relation)
 
-      # Stub pagy to return [pagy_obj, ordered_relation] when called with ordered_relation
-      allow(controller).to receive(:pagy).with(ordered_relation).and_return([pagy_obj, ordered_relation])
+      # Stub pagy
+      allow(controller).to receive(:pagy).and_return([pagy_obj, ordered_relation])
+
+      # Stub order on relation
+      allow(users_relation).to receive(:order).and_return(ordered_relation)
     end
 
     context "when column is 'name' and order is 'asc'" do
-      it 'orders by first_name ASC, last_name ASC and updates @users' do
-        allow(users_relation).to have_received(:order)
-          .with(first_name: :asc, last_name: :asc)
-          .and_return(ordered_relation)
-
+      it 'orders by first_name ASC, last_name ASC' do
         controller.send(:order_by, 'name', 'asc')
 
+        expect(users_relation)
+          .to have_received(:order)
+          .with(first_name: :asc, last_name: :asc)
+
+        expect(controller).to have_received(:pagy).with(ordered_relation)
         expect(controller.instance_variable_get(:@users)).to eq(ordered_relation)
-        expect(controller.instance_variable_get(:@pagy)).to eq(pagy_obj)
       end
     end
 
     context "when column is 'name' and order is 'desc'" do
-      it 'orders by first_name DESC, last_name DESC and updates @users' do
-        allow(users_relation).to have_received(:order)
-          .with(first_name: :desc, last_name: :desc)
-          .and_return(ordered_relation)
-
+      it 'orders by first_name DESC, last_name DESC' do
         controller.send(:order_by, 'name', 'desc')
 
+        expect(users_relation)
+          .to have_received(:order)
+          .with(first_name: :desc, last_name: :desc)
+
+        expect(controller).to have_received(:pagy).with(ordered_relation)
         expect(controller.instance_variable_get(:@users)).to eq(ordered_relation)
-        expect(controller.instance_variable_get(:@pagy)).to eq(pagy_obj)
       end
     end
 
-    context "when column is any other column and order is 'asc'" do
-      it 'orders by ASC"' do
-        column = 'email'
-        allow(users_relation).to have_received(:order)
-          .with("#{column} ASC")
-          .and_return(ordered_relation)
+    context "when a non-name column is given and order is 'asc'" do
+      it 'orders by column ASC' do
+        controller.send(:order_by, 'email', 'asc')
 
-        controller.send(:order_by, column, 'asc')
+        expect(users_relation)
+          .to have_received(:order)
+          .with("email ASC")
 
-        expect(controller.instance_variable_get(:@users)).to eq(ordered_relation)
-        expect(controller.instance_variable_get(:@pagy)).to eq(pagy_obj)
+        expect(controller).to have_received(:pagy).with(ordered_relation)
       end
     end
 
-    context "when column is any other column and order is not 'asc' (e.g., desc)" do
-      it 'orders by DESC"' do
-        column = 'last_sign_in_at'
-        allow(users_relation).to have_received(:order)
-          .with("#{column} DESC")
-          .and_return(ordered_relation)
+    context "when a non-name column is given and order is not asc" do
+      it 'orders by column DESC' do
+        controller.send(:order_by, 'last_sign_in_at', 'desc')
 
-        controller.send(:order_by, column, 'desc')
+        expect(users_relation)
+          .to have_received(:order)
+          .with("last_sign_in_at DESC")
 
-        expect(controller.instance_variable_get(:@users)).to eq(ordered_relation)
-        expect(controller.instance_variable_get(:@pagy)).to eq(pagy_obj)
+        expect(controller).to have_received(:pagy).with(ordered_relation)
       end
     end
   end
