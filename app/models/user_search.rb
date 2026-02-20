9e43df210438617d588f1323111aa7e0b4633e89
Deepthi Doppalapudi <deepthi.doppalapudi@justice.gov.uk>
2026-02-20 11:04:20 +0000

    corrected feature flag options on edit user

--- app/models/user_search.rb
+++ app/models/user_search.rb
@@ -5,13 +5,12 @@ class UserSearch
   attribute :search_string, :string
   attribute :recent_sign_ins, :boolean
   attribute :old_sign_ins, :boolean
-  attribute :manager_role, :boolean
   attribute :caseworker_role, :boolean
   attribute :admin_role, :boolean
   attribute :data_analyst_role, :boolean
 
   def filters_applied?
-    search_string.present? || recent_sign_ins || old_sign_ins || manager_role || caseworker_role ||
+    search_string.present? || recent_sign_ins || old_sign_ins || caseworker_role ||
       admin_role || data_analyst_role
   end
 
@@ -29,10 +28,6 @@ class UserSearch
 
   def old_count
     User.where('last_sign_in_at IS NULL OR last_sign_in_at < ?', 3.months.ago).count
-  end
-
-  def manager_count
-    User.where('roles IS NULL OR ? = ANY(roles)', 'manager').count
   end
 
   def caseworker_count
