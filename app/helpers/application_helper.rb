2127a771ffe7900fcd99da5ecab1a7c8e239d478
Alessandro De Simone <alexdesi@gmail.com>
2026-02-25 09:39:22 +0000

    ACD-1238: Replace moj-pagination with GOV.UK pagination component for hearings and hearing days

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,6 +60,22 @@ module ApplicationHelper
     # rubocop:enable Rails/OutputSafety
   end
 
+  def pagination_svg_icon(direction)
+    # rubocop:disable Layout/LineLength
+    prev_d = "m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"
+    next_d = "m8.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7246z"
+    # rubocop:enable Layout/LineLength
+    path_d = direction == :prev ? prev_d : next_d
+    content_tag(:svg,
+                content_tag(:path, '', d: path_d),
+                class: "govuk-pagination__icon govuk-pagination__icon--#{direction}",
+                xmlns: "http://www.w3.org/2000/svg",
+                height: "13", width: "15",
+                focusable: "false",
+                aria: { hidden: "true" },
+                viewBox: "0 0 15 13")
+  end
+
   private
 
   def decorator_instance(object, decorator_class = nil)
