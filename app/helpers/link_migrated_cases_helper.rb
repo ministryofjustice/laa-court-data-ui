90912c242e62eefbd8c3ba7d39066625c81b7dc9
Alessandro De Simone <alexdesi@gmail.com>
2026-04-22 13:14:42 +0100

    ACD-1360: Implement sortable link migrated cases table view
    Add sortable columns (case_urn, defendant_name, xhibit_case_number, court_name, mode_of_trial) with correct API sort_by parameter names. Fix sort direction to reset to asc when switching columns and toggle when clicking the active column.

---
+++ app/helpers/link_migrated_cases_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module LinkMigratedCasesHelper
+  def link_migrated_cases_sorter_link(column)
+    direction = current_sort_column?(column) && params[:sort_direction] == 'asc' ? 'desc' : 'asc'
+    link_migrated_cases_path(sort_column: column,
+                             sort_direction: direction)
+  end
+
+  def link_migrated_cases_sorter_direction
+    params[:sort_direction] == 'desc' ? 'desc' : 'asc'
+  end
+
+  def current_sort_column?(column)
+    if params[:sort_column].nil?
+      column == 'case_urn'
+    else
+      params[:sort_column] == column
+    end
+  end
+end
