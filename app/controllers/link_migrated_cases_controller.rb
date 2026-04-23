9806a7e087c6b7d68711dfc34833772fd8e85737
Alessandro De Simone <alexdesi@gmail.com>
2026-04-23 13:04:07 +0100

    ACD-1360: Add pagination and stable column widths to link migrated cases table

--- app/controllers/link_migrated_cases_controller.rb
+++ app/controllers/link_migrated_cases_controller.rb
@@ -5,10 +5,15 @@ class LinkMigratedCasesController < ApplicationController
   before_action :check_feature_flag
 
   SORTABLE_COLUMNS = %w[case_urn defendant_name xhibit_case_number court_name mode_of_trial].freeze
+  CASES_PER_PAGE = 10
 
   def index
-    @result = Cda::LinkMigratedCasesService.call(sort_by: sort_col_param, sort_direction: sort_dir_param)
+    @result = Cda::LinkMigratedCasesService.call(sort_by: sort_col_param,
+                                                 sort_direction: sort_dir_param,
+                                                 page: page_param,
+                                                 per_page: CASES_PER_PAGE)
     @cases = @result['results'] || []
+    @pagy = Pagy.new(count: @result['total_results'].to_i, page: page_param, limit: CASES_PER_PAGE)
   end
 
   private
@@ -19,6 +24,10 @@ class LinkMigratedCasesController < ApplicationController
 
   def sort_dir_param
     params[:sort_direction] == 'desc' ? 'desc' : 'asc'
+  end
+
+  def page_param
+    [params[:page].to_i, 1].max
   end
 
   def check_feature_flag
