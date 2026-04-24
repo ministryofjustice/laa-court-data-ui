c72d4f10520995ab1667b48f7e6e74d9c220e639
Alessandro De Simone <alexdesi@gmail.com>
2026-04-24 17:56:35 +0100

    ACD-1360: Add request specs for link migrated cases controller to raise coverage

---
+++ spec/requests/link_migrated_cases_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+RSpec.describe 'link_migrated_cases', type: :request do
+  let(:user) { create(:user, :with_caseworker_role) }
+
+  before do
+    sign_in user
+    allow(FeatureFlag).to receive(:enabled?).and_call_original
+    allow(FeatureFlag).to receive(:enabled?).with(:show_link_migrated_cases).and_return(true)
+  end
+
+  context 'when a non-need_linking tab is requested', :stub_oauth_token, :stub_link_migrated_cases do
+    it 'renders the index with an empty cases list' do
+      get link_migrated_cases_path(tab: 'manually_linked')
+      expect(response).to be_successful
+    end
+  end
+
+  context 'when the feature flag is disabled' do
+    it 'redirects to the user root' do
+      allow(FeatureFlag).to receive(:enabled?).with(:show_link_migrated_cases).and_return(false)
+      get link_migrated_cases_path
+      expect(response).to redirect_to(authenticated_root_path)
+    end
+  end
+end
