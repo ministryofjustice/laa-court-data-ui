ec20d9d1d8434b30b927df48dbc0ceb6fad39318
Alessandro De Simone <alexdesi@gmail.com>
2026-06-30 17:25:26 +0100

    Update unlinking reason seeds, and update seeds logic to update the descriptions.

--- app/models/unlink_reason.rb
+++ app/models/unlink_reason.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class UnlinkReason < ApplicationRecord
+  def self.ordered
+    all.sort_by { |r| [r.description == 'Other' ? 1 : 0, r.code] }
+  end
+
   validates :code, presence: true, uniqueness: true
   validates :description, presence: true, uniqueness: { case_sensitive: false }
 end
