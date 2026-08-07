54d73525780dfbf31c2275debd225e4b7fdb6862
Stuart Harrison <pezholio@gmail.com>
2026-08-07 14:32:19 +0100

    Fix Breadcrumb builder specs
    By making the test a `helper` spec, we can pass the `helper`
    variable into the builder, and the tests run correctly. As we’re
    doing string matching, weneed to include the `role` and `aria-label` in
    the expectations for the outer `div` test too.

--- spec/lib/gds_design_system_breadcrumb_builder_spec.rb
+++ spec/lib/gds_design_system_breadcrumb_builder_spec.rb
@@ -2,18 +2,18 @@
 
 require 'gds_design_system_breadcrumb_builder'
 
-RSpec.describe GdsDesignSystemBreadcrumbBuilder do
-  subject(:builder) { described_class.new(template, []) }
-
-  let(:template) { Class.new(ActionView::TestCase) }
+RSpec.describe GdsDesignSystemBreadcrumbBuilder, type: :helper do
+  subject(:builder) { described_class.new(helper, []) }
 
   it { is_expected.to respond_to :render, :render_element }
 
-  describe '#render', skip: 'FIX: test template not responding to :tag' do
+  describe '#render' do
     subject(:content) { builder.render }
 
     it 'renders outer <div>' do
-      expect(content).to include('<div class="govuk-breadcrumbs">')
+      expect(content).to include(
+        '<div class="govuk-breadcrumbs" role="navigation" aria-label="Navigate Case">'
+      )
     end
 
     it 'renders order list <ol>' do
