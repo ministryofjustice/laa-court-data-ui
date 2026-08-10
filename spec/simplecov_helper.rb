d98db006e0f7b8d0dab9894eeb62d25a01e4e7a0
Leandro C. <leandro.costantini@justice.gov.uk>
2026-08-10 12:52:15 +0100

    Fix SimpleCov deprecation messages (#3267)

--- spec/simplecov_helper.rb
+++ spec/simplecov_helper.rb
@@ -9,20 +9,20 @@ SimpleCov.configure do
                                                                     SimpleCov::Formatter::JSONFormatter
                                                                   ])
 
-  add_filter '_spec.rb'
-  add_filter 'spec/'
-  add_filter 'config/'
-  add_filter 'db/seeds'
-  add_filter %r{^/factories/}
-  add_filter 'bin/'
-  add_filter 'lib/tasks'
-  add_filter 'rakefile'
+  skip '_spec.rb'
+  skip 'spec/'
+  skip 'config/'
+  skip 'db/seeds'
+  skip %r{^/factories/}
+  skip 'bin/'
+  skip 'lib/tasks'
+  skip 'rakefile'
 
   # exclude individual files/dirs from test coverage stats
-  add_filter 'app/controllers/users/confirmations_controller'
+  skip 'app/controllers/users/confirmations_controller'
 
   # group functionality for test coverage report
-  add_group 'Models', 'app/models'
-  add_group 'Mailers', '/app/mailers'
-  add_group 'Helpers', 'app/helpers'
+  group 'Models', 'app/models'
+  group 'Mailers', '/app/mailers'
+  group 'Helpers', 'app/helpers'
 end
