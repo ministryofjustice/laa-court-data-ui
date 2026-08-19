39716c7cd14b08a2741b090e887d0d5013df8b1c
Leandro Costantini <leandro.costantini@justice.gov.uk>
2026-08-19 11:32:33 +0100

    [ACD-1213] Move category tag to next row on `Migrated cases page` table

--- app/helpers/link_migrated_cases_helper.rb
+++ app/helpers/link_migrated_cases_helper.rb
@@ -95,13 +95,13 @@ private
 
   def handle_case_urn(m_case)
     tag.div(class: "tags") do
-      safe_join([m_case["case_urn"], case_type_tag(m_case["case_type"])].compact, "\n")
+      safe_join([m_case["case_urn"], case_type_tag(m_case["case_type"])].compact, "<br>".html_safe)
     end
   end
 
   def handle_case_urn_new_tab(m_case)
     tag.div(class: "tags") do
-      safe_join([case_urn_new_tab_url(m_case["case_urn"]), case_type_tag(m_case["case_type"])].compact, "\n")
+      safe_join([case_urn_new_tab_url(m_case["case_urn"]), case_type_tag(m_case["case_type"])].compact, "<br>".html_safe)
     end
   end
 
